908035e235cc086eeea1b77ca5f6b277586487e4
Grzegorz Martowski <gmartowski@pgs-soft.com>
2020-09-04 10:09:20 +0200

    STJS-933 Move react example page and adjust js-payments example page

--- example/reactjs-payments/src/components/navigation/navigation.jsx
+++ example/reactjs-payments/src/components/navigation/navigation.jsx
@@ -1,23 +1,25 @@
 import React from 'react';
 import { Link } from 'gatsby';
-import { NavigationLinkStyles } from './navigation.styled';
+import { NavigationLinkStyles, NavigationStyled } from './navigation.styled';
 
 export const Navigation = () => (
-  <ul className="navigation">
-    <Link to="/" style={NavigationLinkStyles}>
-      Home
-    </Link>
-    <Link to="/payment-details" style={NavigationLinkStyles}>
-      Payment Details
-    </Link>
-    <Link to="/personal-data" style={NavigationLinkStyles}>
-      Personal Data
-    </Link>
-    <Link to="/contact-data" style={NavigationLinkStyles}>
-      Contact Data
-    </Link>
-    <Link to="/" style={NavigationLinkStyles}>
-      Payment
-    </Link>
-  </ul>
+  <NavigationStyled>
+    <ul className="navigation">
+      <Link to="/" style={NavigationLinkStyles}>
+        Home
+      </Link>
+      <Link to="/payment-details" style={NavigationLinkStyles}>
+        Payment Details
+      </Link>
+      <Link to="/personal-data" style={NavigationLinkStyles}>
+        Personal Data
+      </Link>
+      <Link to="/contact-data" style={NavigationLinkStyles}>
+        Contact Data
+      </Link>
+      <Link to="/" style={NavigationLinkStyles}>
+        Payment
+      </Link>
+    </ul>
+  </NavigationStyled>
 );
