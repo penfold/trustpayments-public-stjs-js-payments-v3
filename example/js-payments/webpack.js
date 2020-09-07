d152ea28950938eba4ae118cc8cba0c8c338208f
Grzegorz Martowski <gmartowski@pgs-soft.com>
2020-09-07 15:10:35 +0200

    STJS-934 Remove unused import and libraries from js-payments, checking if it works after deletion

--- example/js-payments/webpack.js
+++ example/js-payments/webpack.js
@@ -18,9 +18,9 @@ module.exports = {
     publicPath: '',
     port: 8444,
     https: {
-      key: fs.readFileSync('./../../docker/nginx/cert/merchant.securetrading.net/key.pem'),
-      cert: fs.readFileSync('./../../docker/nginx/cert/merchant.securetrading.net/cert.pem'),
-      ca: fs.readFileSync('./../../docker/nginx/cert/minica.pem')
+      key: fs.readFileSync('./../../docker/app-html/nginx/cert/merchant.securetrading.net/key.pem'),
+      cert: fs.readFileSync('./../../docker/app-html/nginx/cert/merchant.securetrading.net/cert.pem'),
+      ca: fs.readFileSync('./../../docker/app-html/nginx/cert/minica.pem')
     },
     hot: true,
     host: '0.0.0.0',
