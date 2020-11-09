b5b1bac0f6c8e59cfc5f55468f124186275f8965
Marek Sakowicz <msakowicz@pgs-soft.com>
2020-11-09 17:07:07 +0000

    Stjs 1101 update mock config in automation tests

---
+++ tests/utils/enums/responses/jsinit_response.py
@@ -0,0 +1,10 @@
+jsinit_response = {
+    'config_tokenisation_visa': 'jsinitTokenisationVisa.json',
+    'config_tokenisation_bypass_cards_visa': 'jsinitTokenisationVisa.json',
+    'config_tokenisation_visa_request_types': 'jsinitTokenisationVisa.json',
+    'config_tokenisation_amex': 'jsinitTokenisationAmex.json',
+    'subscription': 'jsinitSubscription.json',
+    'start_on_load_sub': 'jsinitStartOnLoadSubscription.json',
+    'start_on_load': 'jsinitStartOnLoad.json',
+    'immediate_payment': 'jsinitStartOnLoad.json'
+}
