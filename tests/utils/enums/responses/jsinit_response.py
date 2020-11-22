2ff34fba3a9a2c67bd0f4de1dac5031d919034e8
Marek Sakowicz <msakowicz@pgs-soft.com>
2020-11-22 12:43:47 +0000

    STJS-1093 Remove config schema for requestTypes and bypass cards, apply a new appproach to get request types and bypassed cards

--- tests/utils/enums/responses/jsinit_response.py
+++ tests/utils/enums/responses/jsinit_response.py
@@ -6,5 +6,16 @@ jsinit_response = {
     'subscription': 'jsinitSubscription.json',
     'start_on_load_sub': 'jsinitStartOnLoadSubscription.json',
     'start_on_load': 'jsinitStartOnLoad.json',
-    'immediate_payment': 'jsinitStartOnLoad.json'
+    'immediate_payment': 'jsinitStartOnLoad.json',
+    'config_requestTypes_tdq': 'jsinitTDQ.json',
+    'config_requestTypes_acheck_tdq_auth': 'jsinitAcheckTdqAuth.json',
+    'config_requestTypes_tdq_auth_riskdec': 'jsinitTdqAuthRiskdec.json',
+    'config_requestTypes_riskdec_acheck_tdq_auth': 'jsinitRiskdecAcheckTdqAuth.json',
+    'config_requestTypes_tdq_submit_on_success': 'jsinitTDQ.json',
+    'config_requestTypes_tdq_acheck_riskdec_auth': 'jsinitTdqAcheckRiskdecAuth.json',
+    'config_requestTypes_acheck_tdq_auth_subscription': 'jsinitAcheckTdqAuthSub.json',
+    'config_immediate_payment_tdq': 'jsinitStartOnLoadTdq.json',
+    'config_immediate_payment_tdq_auth': 'jsinitStartOnLoadTdqAuth.json',
+    'config_immediate_payment_acheck_tdq_auth': 'jsinitStartOnLoadAcheckTdqAuth.json',
+    'config_immediate_payment_riskdec_acheck_tdq_auth': 'jsinitStartOnLoadRiskdecAcheckTdqAuth.json',
 }
