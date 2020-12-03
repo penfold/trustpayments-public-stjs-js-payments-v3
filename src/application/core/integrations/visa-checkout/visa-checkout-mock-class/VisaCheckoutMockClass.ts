11d3ae61d417c838a6742e0ce5131b1ad96c5fea
gbielak <gbielak@pgs-soft.com>
2020-12-03 13:19:14 +0100

    STJS-905 - enable e2e tests

--- src/application/core/integrations/visa-checkout/visa-checkout-mock-class/VisaCheckoutMockClass.ts
+++ src/application/core/integrations/visa-checkout/visa-checkout-mock-class/VisaCheckoutMockClass.ts
@@ -1,4 +1,4 @@
-import { map } from 'rxjs/operators';
+import { map, take } from 'rxjs/operators';
 import { Service } from 'typedi';
 import { environment } from '../../../../../environments/environment';
 import { InterFrameCommunicator } from '../../../../../shared/services/message-bus/InterFrameCommunicator';
@@ -6,6 +6,8 @@ import { ofType } from '../../../../../shared/services/message-bus/operators/ofT
 import { PUBLIC_EVENTS } from '../../../models/constants/EventTypes';
 import { MERCHANT_PARENT_FRAME } from '../../../models/constants/Selectors';
 import { DomMethods } from '../../../shared/dom-methods/DomMethods';
+import { IVisaCheckoutStatusData } from '../visa-checkout-status-data/IVisaCheckoutStatusData';
+import { IVisaCheckoutStatusDataSuccess } from '../visa-checkout-status-data/IVisaCheckoutStatusDataSuccess';
 import { VisaCheckout } from '../VisaCheckout';
 import { VisaCheckoutButtonProps } from '../visa-checkout-button-service/VisaCheckoutButtonProps';
 
@@ -26,23 +28,26 @@ export class VisaCheckoutMockClass {
     });
   }
 
-  private _handleMockedData() {
+  private _handleMockedData(): Promise<void> {
     return fetch(environment.VISA_CHECKOUT_URLS.MOCK_DATA_URL)
-      .then((response: any) => response.json())
+      .then((response: Body) => {
+        return response.json() as IVisaCheckoutStatusData;
+      })
       .then(({ payment, status }: any) => {
         this._proceedFlowWithMockedData(payment, status);
       });
   }
 
-  private _proceedFlowWithMockedData(payment: any, status: string) {
+  private _proceedFlowWithMockedData(data: any, status: string) {
     if (status === 'SUCCESS') {
       this.interFrameCommunicator.incomingEvent$
         .pipe(
           ofType(PUBLIC_EVENTS.VISA_CHECKOUT_START),
+          take(1),
           map(() => {
             return {
               type: PUBLIC_EVENTS.VISA_CHECKOUT_START,
-              data: payment
+              data: data as IVisaCheckoutStatusDataSuccess
             };
           })
         )
