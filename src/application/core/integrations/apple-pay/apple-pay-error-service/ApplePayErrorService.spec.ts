bc8cff73235c17221e901e7d5365d9a3eb8f3632
Grzegorz Martowski <gmartowski@pgs-soft.com>
2020-12-16 17:54:56 +0100

    STJS-1180 ApplePay Error mock and UT

---
+++ src/application/core/integrations/apple-pay/apple-pay-error-service/ApplePayErrorService.spec.ts
@@ -0,0 +1,18 @@
+import { ApplePayErrorService } from './ApplePayErrorService';
+import { when } from 'ts-mockito';
+import { ApplePayErrorCode } from './ApplePayErrorCode';
+import { ApplePayErrorContactField } from './ApplePayErrorContactField';
+
+describe('ApplePayErrorService', () => {
+  it('should create and return new ApplePayError', () => {
+    const instance: ApplePayErrorService = new ApplePayErrorService();
+    when(
+      instance.create(
+        ApplePayErrorCode.ADDRESS_UNSERVICEABLE,
+        'en_GB',
+        ApplePayErrorContactField.POSTAL_CODE,
+        'test message'
+      )
+    ).thenReturn({});
+  });
+});
--- src/application/core/integrations/apple-pay/apple-pay-error-service/ApplePayErrorService.ts
+++ src/application/core/integrations/apple-pay/apple-pay-error-service/ApplePayErrorService.ts
@@ -1,5 +1,6 @@
 import { Service } from 'typedi';
 import { IApplePayError } from './IApplePayError';
+import { ApplePayErrorContactField } from './IApplePayErrorContactField';
 import { Translator } from '../../../shared/translator/Translator';
 import { ApplePayErrorCode } from './ApplePayErrorCode';
 import { Locale } from '../../../shared/translator/Locale';
@@ -8,9 +9,14 @@ const ApplePayError = (window as any).ApplePayError;
 
 @Service()
 export class ApplePayErrorService {
-  create(content: ApplePayErrorCode, locale: Locale): IApplePayError {
+  create(
+    content: ApplePayErrorCode,
+    locale: Locale,
+    contactField?: ApplePayErrorContactField,
+    message?: string
+  ): IApplePayError {
     const translation: Translator = new Translator(locale);
 
-    return new ApplePayError(translation.translate(content));
+    return new ApplePayError(translation.translate(content), contactField, message);
   }
 }
