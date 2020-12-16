0d289ca0839570ced760c237a2e6687c101b3f1c
Grzegorz Martowski <gmartowski@pgs-soft.com>
2020-12-16 16:44:40 +0100

    STJS-1180 Apple pay - second portion of fixes in PR after CR

--- src/application/core/integrations/apple-pay/apple-pay-session-service/ApplePaySessionService.ts
+++ src/application/core/integrations/apple-pay/apple-pay-session-service/ApplePaySessionService.ts
@@ -2,8 +2,8 @@ import { Service } from 'typedi';
 import { IApplePayPaymentMethodSelectedEvent } from '../apple-pay-payment-data/IApplePayPaymentMethodSelectedEvent';
 import { IApplePayPaymentRequest } from '../apple-pay-payment-data/IApplePayPaymentRequest';
 import { IApplePaySession } from './IApplePaySession';
-import { IApplePayShippingMethodSelectedEvent } from '../apple-pay-shipping-data/IApplePayShippingMethodSelectedEvent';
 import { IApplePayShippingContactSelectedEvent } from '../apple-pay-shipping-data/IApplePayShippingContactSelectedEvent';
+import { IApplePayShippingMethodSelectedEvent } from '../apple-pay-shipping-data/IApplePayShippingMethodSelectedEvent';
 
 const ApplePaySession = (window as any).ApplePaySession;
 
