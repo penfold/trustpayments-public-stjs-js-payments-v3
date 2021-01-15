f850f7e9216e8836b4f1dd57e729be5bb80bbe87
gbielak <gbielak@pgs-soft.com>
2021-01-15 16:18:21 +0100

    STJS-1180 - remove ApplePay button link from test mock

--- src/application/core/integrations/apple-pay/apple-pay-button-service/ApplePayButtonServiceMock.ts
+++ src/application/core/integrations/apple-pay/apple-pay-button-service/ApplePayButtonServiceMock.ts
@@ -3,21 +3,6 @@ import { ApplePayButtonService } from './ApplePayButtonService';
 
 export class ApplePayButtonServiceMock extends ApplePayButtonService {
   createButton(label: string, style: string, locale: string): HTMLElement {
-    const buttonImg: HTMLElement = DomMethods.createHtmlElement.apply(this, [
-      { src: './img/apple-pay.png', id: 'st-apple-pay' },
-      'img'
-    ]);
-    const buttonLink: HTMLElement = DomMethods.createHtmlElement.apply(this, [
-      {
-        style: `-webkit-appearance: -apple-pay-button;
-                -apple-pay-button-type: plain;
-                -apple-pay-button-style: black;pointer-events: auto;cursor: pointer;display: flex;role: button;`,
-        lang: 'en_GB'
-      },
-      'a'
-    ]);
-    buttonLink.appendChild(buttonImg);
-
-    return buttonLink;
+    return DomMethods.createHtmlElement.apply(this, [{ src: './img/apple-pay.png', id: 'st-apple-pay' }, 'img']);
   }
 }
