7c9ee0a5722e33eb02c702220dc5f62ed8b07e6a
Jakub Jagoda <jjagoda@pgs-soft.com>
2021-02-15 16:57:04 +0100

    Example page for jumpman cases

--- example/html/pages/jumpman/index.js
+++ example/html/pages/jumpman/index.js
@@ -1,7 +1,88 @@
-(function($) {
-  $(document).ready(function() {
+import 'url-polyfill';
+import '../../styles/style.scss';
+
+import { jwtgenerator } from '@trustpayments/jwt-generator';
+
+const popupStyles =
+  'display: flex; justify-content: center; position: fixed; left: 0; height: 70px; width: 280px;right:0;color: white;padding: 0 40px;align-items: center;border-radius: 10px;font-family: Verdana;font-size: 20px;z-index:2';
+const popup = document.getElementById('st-popup');
+let callbackCounter = 0;
+let submitCallbackCounter = 0;
+let successCallbackCounter = 0;
+let errorCallbackCounter = 0;
+let cancelCallbackCounter = 0;
+
+window.displayCallbackCounter = (id, text, tp) => {
+  const div = document.createElement('div');
+  callbackCounter += 1;
+  if (id === 'submit-callback-counter') {
+    submitCallbackCounter += 1;
+    div.innerText = `${text} Callback counter: ${submitCallbackCounter}`;
+  } else if (id === 'success-callback-counter') {
+    successCallbackCounter += 1;
+    div.innerText = `${text} Callback counter: ${successCallbackCounter}`;
+  } else if (id === 'error-callback-counter') {
+    errorCallbackCounter += 1;
+    div.innerText = `${text} Callback counter: ${errorCallbackCounter}`;
+  } else if (id === 'cancel-callback-counter') {
+    cancelCallbackCounter += 1;
+    div.innerText = `${text} Callback counter: ${cancelCallbackCounter}`;
+  } else {
+    div.innerText = `${text} Callback counter: ${callbackCounter}`;
+  }
+  div.setAttribute('id', id);
+  div.setAttribute('style', popupStyles);
+  div.style.backgroundColor = tp;
+  switch (tp) {
+    case 'blue':
+      div.style.top = '150px';
+      break;
+    default:
+      div.style.top = '250px';
+  }
+  popup.appendChild(div);
+  setTimeout(() => {
+    popup.removeChild(div);
+  }, 8000);
+};
+
+window.configJWT = (url, additionalData) =>
+  fetch(url)
+    .then(response => response.json())
+    .then(out => jwtgenerator({ ...out.payload, ...additionalData }, out.secret, out.iss));
+
+const getResponse = () => {
+  const response = {};
+  const selectedResponseOptions = Array.from($('#additional-effects input:checkbox:checked').map((_, i) => $(i).val()));
+
+  if (selectedResponseOptions.includes('pay-html')) {
+    response.html = [
+      '<div>Appended element</div>',
+      '<button>Appended button</button>'
+    ];
+  }
+
+  if (selectedResponseOptions.includes('pay-eval')) {
+    response.js = response.js || [];
+    response.js.push(
+      '(function() { for (let i = 0; i < 5; i++) { console.log(i); }}())',
+      '(function() { setTimeout(() => console.log("test"), 1000) }())',
+    );
+  }
+
+  if (selectedResponseOptions.includes('pay-eval-err')) {
+    response.js = response.js || [];
+    response.js.push(
+      '(function() { "use strict"; _nonExistingVar = 7; }())',
+    );
+  }
+
+  return response;
+}
+
+(function($){
+  $(document).ready(function(){
     var $body = $('body');
-    var $loader = $('#debit-loader');
 
     var completeHandler = function(response, status) {
       if (response.popup) {
@@ -15,100 +96,47 @@
       });
     };
 
-    var errorHandler = function(response, status) {
-      $loader.hide();
-    };
-    $('#st-form-js').submit(function(event) {
+    $('#st-form').submit(function(event) {
       event.preventDefault();
-      var $form = $(this);
-      if (stIsValid($form)) {
-        $loader.show();
-        $.post({
-          url: $form.attr('action'),
-          data: $form.serialize(),
-          cache: false,
-          success: function(response) {
-            completeHandler(response, status);
-            if (response.result) {
-              var st = SecureTrading({
-                jwt: response.jwt,
-                livestatus: parseInt(response.livestatus),
-                submitOnError: false,
-                submitOnSuccess: false,
-                submitCallback: (function() {
-                  var $form = $('#st-form-js');
-                  var formData = $('#st-form-js, #st-form').serializeArray();
-                  formData.push({ name: 'transaction_id', value: response.transaction_id });
-                  $.post({
-                    url: $form.attr('action'),
-                    data: $.param(formData),
-                    cache: false,
-                    success: function(response, status) {
-                      $('.popup:visible').hide();
-                      completeHandler(response, status);
-                      $loader.hide();
-                      st.destroy();
-                      $('#st-control-frame-iframe').remove();
-                    },
-                    error: function(response, status) {
-                      completeHandler(response, status);
-                      errorHandler(response, status);
-                      st.destroy();
-                      $('#st-control-frame-iframe').remove();
-                    }
-                  });
-                })
-              });
-              st.Components({
-                startOnLoad: true
-              });
-            } else {
-              errorHandler(response, status);
-            }
+      window.getJwt().then((jwt) => {
+        var st = SecureTrading({
+          jwt,
+          livestatus: 0,
+          submitOnError: false,
+          submitOnSuccess: false,
+          submitCallback: (function(data){
+            var stringified = JSON.stringify(data);
+            var testVariable = 'This is what we have got after submit' + stringified;
+            displayPopup('data-popup', 'Error code: ' + (!data || data.errorcode != '0' ? 'Error' : 'OK'), 'blue');
+            displayPopup('data-popup-jwt', 'JWT: ' + data.jwt, 'blue');
+            displayPopup('data-popup-threedresponse', 'THREEDRESPONSE: ' + data.threedresponse, 'blue');
+
+            window.displayCallbackCounter('submit-callback-counter', 'submit', 'blue');
+            console.error(testVariable);
+
+            setTimeout(() => {
+              completeHandler(getResponse());
+              st.destroy();
+              $('#st-control-frame-iframe').remove();
+            }, 1000);
+          }),
+          successCallback: function() {
+            displayPopup('success-popup', 'This is success message', 'green');
+            window.displayCallbackCounter('success-callback-counter', 'success', 'green');
           },
-          error: function(response, status) {
-            completeHandler(response, status);
-            errorHandler(response, status);
-          }
+          errorCallback: function() {
+            displayPopup('error-popup', 'This is error message', 'red');
+            window.displayCallbackCounter('error-callback-counter', 'error', 'red');
+          },
+          cancelCallback: function() {
+            displayPopup('cancel-popup', 'This is cancel message', '#ffc23a');
+            window.displayCallbackCounter('cancel-callback-counter', 'cancel', '#ffc23a');
+          },
         });
-      }
+        st.Components({
+          startOnLoad: true,
+        });
+      })
     });
-
-    function stIsValid($form) {
-      var isValid = true;
-      var $pan = $('input[name="pan"]', $form);
-      var $panError = $('#reg_stpan_div');
-      var $expiryMonth = $('select[name="expirymonth"]', $form);
-      var $expiryYear = $('select[name="expiryyear"]', $form);
-      var $expiryError = $('#reg_stexpiry_div');
-      var $cvv = $('input[name="cvv"]', $form);
-      var $cvvError = $('#reg_stcvv_div');
-
-      if ($pan.length > 0 && '' === $pan.val()) {
-        isValid = false;
-        $panError.addClass('active');
-      } else {
-        $panError.removeClass('active');
-      }
-
-      if (
-        $expiryMonth.length > 0 && '' === $expiryMonth.val() &&
-        $expiryYear.length > 0 && '' === $expiryYear.val()
-      ) {
-        isValid = false;
-        $expiryError.addClass('active');
-      } else {
-        $expiryError.removeClass('active');
-      }
-
-      if ($cvv.length > 0 && '' === $cvv.val()) {
-        isValid = false;
-        $cvvError.addClass('active');
-      } else {
-        $cvvError.removeClass('active');
-      }
-
-      return isValid;
-    };
   });
 })(jQuery);
