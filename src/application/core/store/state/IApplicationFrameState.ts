fd21d854a03101b4d5124fe186c4d46cede437b2
Piotr Dobrowolski <pdobrowolski@pgs-soft.com>
2021-03-11 07:55:42 +0000

    STJS-1349 Separate RequestEncoder class

--- src/application/core/store/state/IApplicationFrameState.ts
+++ src/application/core/store/state/IApplicationFrameState.ts
@@ -1,6 +1,7 @@
 import { IConfig } from '../../../../shared/model/config/IConfig';
 
 export interface IApplicationFrameState {
+  jwt?: string;
   config?: IConfig;
   storage: { [key: string]: any };
   applePay?: { [key: string]: any };
