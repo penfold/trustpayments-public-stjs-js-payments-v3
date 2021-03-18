b49b6269e65a71fa4ec948ff0d085ff2f1a3b63a
Jakub Jagoda <jjagoda@pgs-soft.com>
2021-03-18 15:34:18 +0100

    Set up ESLint

---
+++ .eslintrc.js
@@ -0,0 +1,24 @@
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint', 'jest'],
+  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'plugin:jest/recommended'],
+  env: {
+    browser: true,
+    node: true,
+    jest: true,
+  },
+  rules: {
+    'comma-dangle': ['error', 'only-multiline'],
+    '@typescript-eslint/no-unused-vars': 0,
+    '@typescript-eslint/no-var-requires': 0,
+    '@typescript-eslint/no-empty-function': 0,
+    '@typescript-eslint/no-explicit-any': 0,
+    '@typescript-eslint/ban-ts-comment': 0,
+    '@typescript-eslint/no-inferrable-types': 0,
+    'jest/no-done-callback': 0,
+    'jest/no-jasmine-globals': 0,
+    'object-curly-spacing': [2, 'always'],
+    'quotes': [2, 'single'],
+  },
+};
