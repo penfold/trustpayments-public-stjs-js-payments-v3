bf02f3b5c47d978976bd6d8e74039fb7a8af022a
pdobrowolski <pdobrowolski@pgs-soft.com>
2021-03-22 09:53:23 +0100

    Allow to create demo payment

--- src/client/integrations/google-pay/GooglePay.ts
+++ src/client/integrations/google-pay/GooglePay.ts
@@ -2,14 +2,11 @@ import { Service } from 'typedi';
 import { environment } from '../../../environments/environment';
 import { DomMethods } from '../../../application/core/shared/dom-methods/DomMethods';
 
-const GooglePaySession = (window as any).GooglePaySession;
-
 @Service()
 export class GooglePay {
-  private static readonly SCRIPT_ADDRESS = environment.GOOGLE_PAY.GOOGLE_PAY_URL;
+  private SCRIPT_ADDRESS = environment.GOOGLE_PAY.GOOGLE_PAY_URL;
   private DOM_TARGET: string = 'st-google-pay';
-  private static SCRIPT_TARGET: string = 'head';
-  private googlePlayClient: any;
+  private SCRIPT_TARGET: string = 'head';
   private baseRequest: any = {
     apiVersion: 2,
     apiVersionMinor: 0
@@ -21,8 +18,8 @@ export class GooglePay {
       gatewayMerchantId: 'exampleGatewayMerchantId'
     }
   };
-  private allowedCardNetworks = ['AMEX', 'DISCOVER', 'INTERAC', 'JCB', 'MASTERCARD', 'VISA'];
   private allowedCardAuthMethods = ['PAN_ONLY', 'CRYPTOGRAM_3DS'];
+  private allowedCardNetworks = ['AMEX', 'DISCOVER', 'INTERAC', 'JCB', 'MASTERCARD', 'VISA'];
   private baseCardPaymentMethod = {
     type: 'CARD',
     parameters: {
@@ -30,39 +27,109 @@ export class GooglePay {
       allowedCardNetworks: this.allowedCardNetworks
     }
   };
-  private isReadyToPayRequest = Object.assign({}, this.baseRequest);
+  private cardPaymentMethod = Object.assign(
+    { tokenizationSpecification: this.tokenizationSpecification },
+    this.baseCardPaymentMethod
+  );
+  private googlePayClient: any = null;
 
   constructor() {
-    this.isReadyToPayRequest.allowedPaymentMethods = [this.baseCardPaymentMethod];
-    this.onInit();
+    this.init();
   }
 
-  private onInit() {
+  private init() {
     this.insertGooglePayLibrary().then(() => {
-      this.googlePlayClient = new (window as any).google.payments.api.PaymentsClient({ environment: 'TEST' });
-      this.ifGooglePayIsAvailable();
+      this.onGooglePayLoaded();
     });
   }
 
   private insertGooglePayLibrary(): Promise<Element> {
-    return DomMethods.insertScript(GooglePay.SCRIPT_TARGET, { src: GooglePay.SCRIPT_ADDRESS });
+    return DomMethods.insertScript(this.SCRIPT_TARGET, { src: this.SCRIPT_ADDRESS });
   }
 
-  protected ifGooglePayIsAvailable(): any {
-    this.googlePlayClient
-      .isReadyToPay(this.isReadyToPayRequest)
+  private onGooglePayLoaded() {
+    const paymentsClient = this.getGooglePaymentsClient();
+    paymentsClient
+      .isReadyToPay(this.getGoogleIsReadyToPayRequest())
       .then((response: any) => {
         if (response.result) {
-          const button = this.googlePlayClient.createButton({ onClick: () => console.log('Start płatności') });
-          document.getElementById(this.DOM_TARGET).appendChild(button);
-          return true;
+          this.addGooglePayButton();
         }
       })
       .catch((err: any) => {
-        // show error in developer console for debugging
-        console.log('nie działa');
         console.error(err);
-        return true;
       });
   }
+
+  private getGoogleIsReadyToPayRequest() {
+    return Object.assign({}, this.baseRequest, {
+      allowedPaymentMethods: [this.baseCardPaymentMethod]
+    });
+  }
+
+  private addGooglePayButton() {
+    const paymentsClient = this.getGooglePaymentsClient();
+    const button = paymentsClient.createButton({ onClick: this.onGooglePaymentButtonClicked });
+    document.getElementById(this.DOM_TARGET).appendChild(button);
+  }
+
+  private getGooglePaymentsClient() {
+    if (this.googlePayClient === null) {
+      this.googlePayClient = new (window as any).google.payments.api.PaymentsClient({ environment: 'TEST' });
+    }
+    return this.googlePayClient;
+  }
+
+  private getGooglePaymentDataRequest() {
+    const paymentDataRequest = Object.assign({}, this.baseRequest);
+    paymentDataRequest.allowedPaymentMethods = [this.cardPaymentMethod];
+    paymentDataRequest.transactionInfo = this.getGoogleTransactionInfo();
+    paymentDataRequest.merchantInfo = {
+      merchantName: 'Example Merchant',
+      merchantId: '12345678901234567890'
+    };
+    return paymentDataRequest;
+  }
+
+  private getGoogleTransactionInfo() {
+    return {
+      countryCode: 'US',
+      currencyCode: 'USD',
+      totalPriceStatus: 'FINAL',
+      totalPrice: '1.00'
+    };
+  }
+
+  private onGooglePaymentButtonClicked = () => {
+    const paymentDataRequest = this.getGooglePaymentDataRequest();
+    paymentDataRequest.transactionInfo = this.getGoogleTransactionInfo();
+    const paymentsClient = this.getGooglePaymentsClient();
+
+    if (paymentsClient) {
+      paymentsClient
+        .loadPaymentData(paymentDataRequest)
+        .then((paymentData: any) => {
+          this.processPayment(paymentData);
+        })
+        .catch((err: any) => {
+          console.error(err);
+        });
+    }
+  };
+
+  private processPayment(paymentData: any) {
+    console.log(paymentData);
+    console.log(paymentData.paymentMethodData.tokenizationData.token);
+  }
+
+  // TO PREFETCH DATA
+  // private prefetchGooglePaymentData() {
+  //   const paymentDataRequest = this.getGooglePaymentDataRequest();
+  //   paymentDataRequest.transactionInfo = {
+  //     totalPriceStatus: 'NOT_CURRENTLY_KNOWN',
+  //     currencyCode: 'USD'
+  //   };
+  //   const paymentsClient = this.getGooglePaymentsClient();
+  //   paymentsClient.prefetchPaymentData(paymentDataRequest);
+  // }
 }
