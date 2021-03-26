6cfe577e702351ac97399e5b33fdbaab950f1c2b
Marek Sakowicz (PGS Software) <msakowicz@pgs-soft.com>
2021-03-26 09:58:40 +0100

    fix action name in payment_method class

--- tests/pages/payment_methods_page.py
+++ tests/pages/payment_methods_page.py
@@ -109,26 +109,26 @@ class PaymentMethodsPage(BasePage):
         self.select_proper_cardinal_authentication(auth)
 
     def fill_cardinal_v1_popup(self):
-        self._action.switch_to_frame(PaymentMethodsLocators.cardinal_v1_iframe)
-        self._executor.wait_for_element_to_be_displayed(
+        self._actions.switch_to_frame(PaymentMethodsLocators.cardinal_v1_iframe)
+        self._waits.wait_for_element_to_be_displayed(
             PaymentMethodsLocators.cardinal_v1_authentication_code_field)
-        self._action.send_keys(PaymentMethodsLocators.cardinal_v1_authentication_code_field,
+        self._actions.send_keys(PaymentMethodsLocators.cardinal_v1_authentication_code_field,
                                AuthData.PASSWORD.value)
         if 'Firefox' in CONFIGURATION.BROWSER:
-            self._action.click_by_javascript(PaymentMethodsLocators.cardinal_v1_authentication_submit_btn)
+            self._actions.click_by_javascript(PaymentMethodsLocators.cardinal_v1_authentication_submit_btn)
         else:
-            self._action.click(PaymentMethodsLocators.cardinal_v1_authentication_submit_btn)
+            self._actions.click(PaymentMethodsLocators.cardinal_v1_authentication_submit_btn)
 
     def fill_cardinal_v2_popup(self):
-        self._executor.wait_for_element_to_be_displayed(
+        self._waits.wait_for_element_to_be_displayed(
             PaymentMethodsLocators.cardinal_v2_authentication_code_field)
-        self._action.send_keys(PaymentMethodsLocators.cardinal_v2_authentication_code_field,
+        self._actions.send_keys(PaymentMethodsLocators.cardinal_v2_authentication_code_field,
                                AuthData.PASSWORD.value)
         self.scroll_to_bottom()
         if 'Firefox' in CONFIGURATION.BROWSER:
-            self._action.click_by_javascript(PaymentMethodsLocators.cardinal_v2_authentication_submit_btn)
+            self._actions.click_by_javascript(PaymentMethodsLocators.cardinal_v2_authentication_submit_btn)
         else:
-            self._action.click(PaymentMethodsLocators.cardinal_v2_authentication_submit_btn)
+            self._actions.click(PaymentMethodsLocators.cardinal_v2_authentication_submit_btn)
 
     def validate_cardinal_authentication_modal_appears(self, auth):
         self._actions.switch_to_iframe(FieldType.CARDINAL_IFRAME.value)
@@ -143,7 +143,7 @@ class PaymentMethodsPage(BasePage):
 
     def select_proper_cardinal_authentication(self, auth):
         self._waits.wait_for_element_to_be_displayed(PaymentMethodsLocators.cardinal_modal)
-        self._action.switch_to_frame(PaymentMethodsLocators.cardinal_iframe)
+        self._actions.switch_to_frame(PaymentMethodsLocators.cardinal_iframe)
         if auth == AuthType.V1.value:
             self.fill_cardinal_v1_popup()
         else:
