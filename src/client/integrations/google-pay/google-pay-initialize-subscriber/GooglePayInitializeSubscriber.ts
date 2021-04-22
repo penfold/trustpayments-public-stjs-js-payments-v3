0aa4b12e5bb9aa8447bde10f4622cb2ad8cfcf56
Grzegorz <kontakt@grzegorzbielak.pl>
2021-04-22 14:20:17 +0200

    STJS-1526 filter google name + better typings + unit test

--- src/client/integrations/google-pay/google-pay-initialize-subscriber/GooglePayInitializeSubscriber.ts
+++ src/client/integrations/google-pay/google-pay-initialize-subscriber/GooglePayInitializeSubscriber.ts
@@ -1,9 +1,13 @@
 import { Observable } from 'rxjs';
-import { takeUntil } from 'rxjs/operators';
+import { filter, takeUntil } from 'rxjs/operators';
 import { Service } from 'typedi';
 import { PUBLIC_EVENTS } from '../../../../application/core/models/constants/EventTypes';
+import { IMessageBusEvent } from '../../../../application/core/models/IMessageBusEvent';
+import { IInitPaymentMethod } from '../../../../application/core/services/payments/events/IInitPaymentMethod';
 import { IMessageBus } from '../../../../application/core/shared/message-bus/IMessageBus';
+import { GooglePaymentMethodName } from '../../../../integrations/google-pay/models/IGooglePaymentMethod';
 import { MessageSubscriberToken } from '../../../../shared/dependency-injection/InjectionTokens';
+import { IConfig } from '../../../../shared/model/config/IConfig';
 import { IMessageSubscriber } from '../../../../shared/services/message-bus/interfaces/IMessageSubscriber';
 import { ofType } from '../../../../shared/services/message-bus/operators/ofType';
 import { GooglePay } from '../GooglePay';
@@ -12,13 +16,19 @@ import { GooglePay } from '../GooglePay';
 export class GooglePayInitializeSubscriber implements IMessageSubscriber {
   constructor(private googlePay: GooglePay) {}
 
-  register(messageBus: IMessageBus) {
+  register(messageBus: IMessageBus): void {
     const destroy$: Observable<void> = messageBus.pipe(ofType(PUBLIC_EVENTS.DESTROY));
 
     messageBus
-      .pipe(ofType(PUBLIC_EVENTS.INIT_PAYMENT_METHOD), takeUntil(destroy$))
-      .subscribe(config => {
-        this.googlePay.init(config);
+      .pipe(
+        ofType(PUBLIC_EVENTS.INIT_PAYMENT_METHOD),
+        filter((event: IMessageBusEvent<IInitPaymentMethod<IConfig>>) => {
+          return event.data.name === GooglePaymentMethodName;
+        }),
+        takeUntil(destroy$),
+      )
+      .subscribe((event: IMessageBusEvent<IInitPaymentMethod<IConfig>>) => {
+        this.googlePay.init(event.data.config);
       });
   }
 }
