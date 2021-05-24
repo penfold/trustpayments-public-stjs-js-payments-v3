3e606692d85131a3c77b2d979791b2a6a20be6b0
Michał Iwańczyk <miwanczyk@pgs-soft.com>
2021-05-24 22:47:20 +0200

    rename locator

--- tests/pages/locators/three_ds_locators.py
+++ tests/pages/locators/three_ds_locators.py
@@ -11,6 +11,6 @@ class ThreeDSMethodsLocators:
 
     three_ds_iframe: By = (By.ID, 'tp-3ds-challenge-iframe')
     three_ds_challenge_input: By = (By.ID, 'challengeDataEntry')
-    three_ds_challenge_cancel_button: By = (By.ID, 'tp-3ds-inline-view-header-button')
+    three_ds_challenge_inline_cancel_button: By = (By.ID, 'tp-3ds-inline-view-header-button')
     three_ds_challenge_submit_button: By = (By.ID, 'tp-3ds-challenge-submit')
     three_ds_challenge_popup_cancel_button: By = (By.ID, 'tp-3ds-popup-view-header-button')
