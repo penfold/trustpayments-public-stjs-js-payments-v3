a73077ec96b5e4d725881d65d3d8dc347246cfe0
Michał Iwańczyk <miwanczyk@pgs-soft.com>
2021-05-26 15:13:26 +0200

    Challenge request scenarios

--- tests/pages/locators/three_ds_locators.py
+++ tests/pages/locators/three_ds_locators.py
@@ -14,3 +14,4 @@ class ThreeDSMethodsLocators:
     three_ds_challenge_inline_cancel_button: By = (By.ID, 'tp-3ds-inline-view-header-button')
     three_ds_challenge_submit_button: By = (By.ID, 'tp-3ds-challenge-submit')
     three_ds_challenge_popup_cancel_button: By = (By.ID, 'tp-3ds-popup-view-header-button')
+    three_ds_invalid_code_label: By = (By.XPATH, '//p[text()="Invalid confirmation code"]')
