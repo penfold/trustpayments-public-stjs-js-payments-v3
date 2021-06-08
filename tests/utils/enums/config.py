4779121045aca49de886b5b4d207a8a0a9deacdf
bdabrowski02 <bdabrowski@pgs-soft.com>
2021-06-08 15:16:31 +0200

    small refactor

--- tests/utils/enums/config.py
+++ tests/utils/enums/config.py
@@ -89,8 +89,5 @@ screenshots = {
     'scrn_card_interface_before_payment_styling': 'cardInterfaceBeforePaymentWithStyling.png',
     'scrn_card_interface_error_expiry_date_styling': 'cardInterfaceErrorExpiryDateWithStyling.png',
     'scrn_card_interface_error_invalid_patterns_styling': 'cardInterfaceErrorInvalidPatternsWithStyling.png',
-    'scrn_card_interface_with_validation_styling': 'cardInterfaceWithValidationStyling.png',
-
-    'scrn_three_ds_processing_screen_inline_attach_to_element': 'threeDsProcessingScreenInlineAttachToElement.png'
-
+    'scrn_card_interface_with_validation_styling': 'cardInterfaceWithValidationStyling.png'
 }
