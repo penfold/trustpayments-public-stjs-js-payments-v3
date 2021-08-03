44bfda08fbcd77052745f12af8c94707f2121b78
Maciej Krawczyk <mackrawczyk@pgs-soft.com>
2021-08-03 08:40:27 +0200

    fixed payment controller so it returns error result on caught errors

---
+++ src/application/core/services/payments/ErrorResultFactory.spec.ts
@@ -0,0 +1,55 @@
+import { ErrorResultFactory } from './ErrorResultFactory';
+import { IRequestTypeResponse } from '../st-codec/interfaces/IRequestTypeResponse';
+import { IPaymentResult } from './IPaymentResult';
+import { PaymentStatus } from './PaymentStatus';
+import { PAYMENT_ERROR } from '../../models/constants/Translations';
+
+describe('ErrorResultFactory', () => {
+  let errorResultFactory: ErrorResultFactory;
+
+  beforeAll(() => {
+    errorResultFactory = new ErrorResultFactory();
+  });
+
+  describe('createResultFromError()', () => {
+    it('should create an error result from gateway response obj', () => {
+      const response: IRequestTypeResponse = {
+        errormessage: 'some error message',
+        errorcode: '12345',
+        requesttypedescription: '',
+        transactionstartedtimestamp: '',
+        customeroutput: '',
+        errordata: '',
+      };
+
+      const result = errorResultFactory.createResultFromError(response);
+      const expectedResult: IPaymentResult<IRequestTypeResponse> = {
+        status: PaymentStatus.ERROR,
+        data: response,
+        error: {
+          code: 12345,
+          message: 'some error message',
+        },
+      };
+
+      expect(result).toEqual(expectedResult);
+    });
+
+    it.each([
+      'error',
+      { foo: 'bar' },
+      new Error('error'),
+    ])('should create an error result from any type of data', errorData => {
+      const expectedResult: IPaymentResult<typeof errorData> = {
+        status: PaymentStatus.ERROR,
+        data: errorData,
+        error: {
+          code: 50003,
+          message: PAYMENT_ERROR,
+        },
+      };
+
+      expect(errorResultFactory.createResultFromError(errorData)).toEqual(expectedResult);
+    });
+  });
+});
