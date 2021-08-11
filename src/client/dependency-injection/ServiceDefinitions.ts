db18dc0fb5d6dff9c538738d21e45ecbab1cb9ea
Wojciech Grabowski (PGS Software) <wgrabowski@pgs-soft.com>
2021-08-11 15:51:44 +0200

    STJS-1930 Inject abstract class for Google pay sdk provider

--- src/client/dependency-injection/ServiceDefinitions.ts
+++ src/client/dependency-injection/ServiceDefinitions.ts
@@ -2,6 +2,8 @@ import { Container } from 'typedi';
 import { ConfigProvider } from '../../shared/services/config-provider/ConfigProvider';
 import { ConfigService } from '../../shared/services/config-service/ConfigService';
 import { GooglePayInitializeSubscriber } from '../integrations/google-pay/google-pay-initialize-subscriber/GooglePayInitializeSubscriber';
+import { GooglePaySdkProvider } from '../integrations/google-pay/google-pay-sdk-provider/GooglePaySdkProvider';
+import { IGooglePaySdkProvider } from '../integrations/google-pay/google-pay-sdk-provider/IGooglePaySdkProvider';
 import { PreventNavigationPopup } from '../message-subscribers/PreventNavigationPopup';
 import { PaymentResultSubmitterSubscriber } from '../common-frames/PaymentResultSubmitterSubscriber';
 import { ThreeDSecureFactory } from '@trustpayments/3ds-sdk-js';
@@ -9,6 +11,7 @@ import '../../shared/dependency-injection/ServiceDefinitions';
 
 Container.set({ id: ConfigProvider, factory: () => Container.get(ConfigService) });
 Container.set({ id: ThreeDSecureFactory, type: ThreeDSecureFactory });
+Container.set({ id: IGooglePaySdkProvider, type: GooglePaySdkProvider });
 Container.import([
   PreventNavigationPopup,
   PaymentResultSubmitterSubscriber,
