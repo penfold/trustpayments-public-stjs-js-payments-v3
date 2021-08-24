4a69cc0bcc2e9ec83e68ddc7a3a9a5e9c2fc6a5e
Piotr Dobrowolski <pdobrowolski99@gmail.com>
2021-08-24 09:11:15 +0200

    Fix dev console warnings

--- src/shared/interfaces/IControlFrameWindow.ts
+++ src/shared/interfaces/IControlFrameWindow.ts
@@ -2,7 +2,7 @@ import { BehaviorSubject, Observable } from 'rxjs';
 import { IMessageBusEvent } from '../../application/core/models/IMessageBusEvent';
 import { IApplicationFrameState } from '../../application/core/store/state/IApplicationFrameState';
 
-export interface IControlFrameWindow extends Window {
+export default interface IControlFrameWindow extends Window {
   stStore?: BehaviorSubject<IApplicationFrameState>;
   stMessages?: Observable<IMessageBusEvent>;
 }
