ed7dd96669eeebb16779c53bc907e4999d619a90
Grzegorz Martowski <gmartowski@pgs-soft.com>
2021-08-25 16:00:28 +0200

    STJS-1120 Delete underscore prefix in whole repo

--- src/application/core/shared/styler/Styler.ts
+++ src/application/core/shared/styler/Styler.ts
@@ -18,7 +18,7 @@ interface StylesAttributes {
 
 @Service()
 export class Styler {
-  private static _getTagStyles(styles: ISubStyles): string {
+  private static getTagStyles(styles: ISubStyles): string {
     const results = [];
     for (const style in styles) {
       results.push(`${style}: ${styles[style]};`);
@@ -26,17 +26,17 @@ export class Styler {
     return results.join(' ');
   }
 
-  private _frame: Frame;
-  private readonly _allowed: IAllowedStyles;
+  private frame: Frame;
+  private readonly allowed: IAllowedStyles;
 
   constructor(allowed: IAllowedStyles, styles: IStyles[]) {
-    this._frame = Container.get(Frame);
-    this._allowed = allowed;
+    this.frame = Container.get(Frame);
+    this.allowed = allowed;
     this.inject(styles);
   }
 
   public inject(styles: IStyles[]): void {
-    DomMethods.insertStyle(this._getStyleString(styles));
+    DomMethods.insertStyle(this.getStyleString(styles));
   }
 
   public hasSpecificStyle(selectedStyle: string, styles: IStyle = {}): boolean {
@@ -64,11 +64,11 @@ export class Styler {
     }
   }
 
-  private _filter(styles: IStyles[]): IStyle {
+  private filter(styles: IStyles[]): IStyle {
     const filtered: IStyle = {};
     styles.forEach((style: IStyle, index) => {
       const propName: string = Object.keys(style)[0];
-      if (Object.prototype.hasOwnProperty.call(this._allowed, propName)) {
+      if (Object.prototype.hasOwnProperty.call(this.allowed, propName)) {
         // @ts-ignore
         filtered[propName] = styles[index][propName];
       }
@@ -76,7 +76,7 @@ export class Styler {
     return filtered;
   }
 
-  private _sanitize(styles: IStyle): IStyle {
+  private sanitize(styles: IStyle): IStyle {
     const sanitized: IStyle = {};
     for (const style in styles) {
       if (/^[A-Za-z0-9 _%#)(,.-]*[A-Za-z0-9][A-Za-z0-9 _%#)(,.-]*$/i.test(styles[style])) {
@@ -86,10 +86,10 @@ export class Styler {
     return sanitized;
   }
 
-  private _group(styles: IStyle): IGroupedStyles {
+  private group(styles: IStyle): IGroupedStyles {
     const grouped: IGroupedStyles = {};
     for (const style in styles) {
-      const allowed = this._allowed[style];
+      const allowed = this.allowed[style];
       if (!Object.prototype.hasOwnProperty.call(grouped, allowed.selector)) {
         grouped[allowed.selector] = {};
       }
@@ -98,13 +98,13 @@ export class Styler {
     return grouped;
   }
 
-  private _getStyleString(styles: IStyles[]): string[] {
-    const styled: IStyle = this._sanitize(this._filter(styles));
-    const groupedStyles: IGroupedStyles = this._group(styled);
+  private getStyleString(styles: IStyles[]): string[] {
+    const styled: IStyle = this.sanitize(this.filter(styles));
+    const groupedStyles: IGroupedStyles = this.group(styled);
     let tag: string;
     const templates: string[] = ['body { display: block; }'];
     for (tag in groupedStyles) {
-      const tagStyle = Styler._getTagStyles(groupedStyles[tag]);
+      const tagStyle = Styler.getTagStyles(groupedStyles[tag]);
       templates.push(`${tag} { ${tagStyle} }`);
     }
     return templates;
