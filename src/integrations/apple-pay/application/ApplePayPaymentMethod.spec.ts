c4a12ffcc833813881508eb70f87bad60588b6f9
Lukasz <lukasz@AdminPGSs-MacBook-Pro.local>
2021-09-03 09:31:07 +0200

    unit tests for ApplePayPaymentMethod

---
+++ src/integrations/apple-pay/application/ApplePayPaymentMethod.spec.ts
@@ -0,0 +1,74 @@
+import { EMPTY, from, of } from 'rxjs';
+import { anything, instance, mock, verify, when } from 'ts-mockito';
+import { PUBLIC_EVENTS } from '../../../application/core/models/constants/EventTypes';
+import { MERCHANT_PARENT_FRAME } from '../../../application/core/models/constants/Selectors';
+import { IMessageBusEvent } from '../../../application/core/models/IMessageBusEvent';
+import { RequestProcessingInitializer } from '../../../application/core/services/request-processor/RequestProcessingInitializer';
+import { IConfig } from '../../../shared/model/config/IConfig';
+import { ConfigProvider } from '../../../shared/services/config-provider/ConfigProvider';
+import { InterFrameCommunicator } from '../../../shared/services/message-bus/InterFrameCommunicator';
+import { ApplePayPaymentMethod } from './ApplePayPaymentMethod';
+
+describe('ApplePayPaymentMethod', () => {
+  const configMock: IConfig = {
+    applePay: {
+      buttonStyle: 'white-outline',
+      buttonText: 'buy',
+      merchantId: 'merchant.net.securetrading.test',
+      paymentRequest: {
+        countryCode: 'US',
+        currencyCode: 'USD',
+        merchantCapabilities: [
+          'supports3DS',
+          'supportsCredit',
+          'supportsDebit'
+        ],
+        supportedNetworks: ['visa', 'amex'],
+        total: {
+        label: 'Secure Trading Merchant',
+        amount: '10.00'
+      }
+    },
+    placement: 'st-apple-pay'
+    }
+  };
+
+  let applePayPaymentMethod: ApplePayPaymentMethod;
+  let requestProcessingInitializerMock: RequestProcessingInitializer;
+  let interFrameCommunicatorMock: InterFrameCommunicator;
+  let configProviderMock: ConfigProvider;
+
+  beforeEach(() => {
+    requestProcessingInitializerMock = mock(RequestProcessingInitializer);
+    interFrameCommunicatorMock = mock(InterFrameCommunicator);
+    configProviderMock = mock<ConfigProvider>();
+
+    applePayPaymentMethod = new ApplePayPaymentMethod(
+      instance(requestProcessingInitializerMock),
+      instance(interFrameCommunicatorMock),
+      instance(configProviderMock),
+    )
+  });
+
+  describe('getName()', () => {
+    it('returns main name of ApplePay service', () => {
+      expect(applePayPaymentMethod.getName()).toBe('ApplePay2');
+    });
+  });
+
+  describe('init()', () => {
+    it('should send an event to initialize payment by the client side', (done) => {
+      when(requestProcessingInitializerMock.initialize()).thenReturn(of(null));
+      when(interFrameCommunicatorMock.query(anything(), anything())).thenReturn(new Promise((resolve) => resolve(null)));
+      when(configProviderMock.getConfig()).thenReturn(configMock);
+
+      applePayPaymentMethod.init().subscribe(
+        () => {
+          verify(requestProcessingInitializerMock.initialize()).once();
+          verify(interFrameCommunicatorMock.query(anything(), MERCHANT_PARENT_FRAME)).once();
+          done();
+        }
+      )
+    });
+  });
+});
