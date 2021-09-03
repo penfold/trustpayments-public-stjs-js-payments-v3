0fddd6e8dfa171702725728da01c453202d66e0e
Lukasz <lukasz@AdminPGSs-MacBook-Pro.local>
2021-09-03 14:54:36 +0200

    unit tests for ApplePayPayClientInitializer

---
+++ src/integrations/apple-pay/client/ApplePayClientInitializer.spec.ts
@@ -0,0 +1,40 @@
+import { Observable } from 'rxjs';
+import { anything, instance, mock, verify, when } from 'ts-mockito';
+import { PUBLIC_EVENTS } from '../../../application/core/models/constants/EventTypes';
+import { InterFrameCommunicator } from '../../../shared/services/message-bus/InterFrameCommunicator';
+import { ApplePayClient } from './ApplePayClient';
+import { ApplePayClientInitializer } from './ApplePayClientInitializer';
+
+describe('ApplePayClientInitializer', () => {
+  const respondMock = jest.fn().mockImplementation((f, obj, cb) => {
+    cb();
+  });
+  let applePayClientInitializer: ApplePayClientInitializer;
+  let applePayClientMock: ApplePayClient;
+  let interFrameCommunicatorMock: InterFrameCommunicator;
+
+  beforeEach(() => {
+    applePayClientMock = mock(ApplePayClient);
+    interFrameCommunicatorMock = mock(InterFrameCommunicator);
+
+    applePayClientInitializer = new ApplePayClientInitializer(
+      instance(applePayClientMock),
+      instance(interFrameCommunicatorMock),
+    );
+  });
+
+  describe('register()', () => {
+    it('checks if applePayClient service has been called', () => {
+      when(interFrameCommunicatorMock.whenReceive(PUBLIC_EVENTS.APPLE_PAY_INIT_CLIENT)).thenCall((eventType = { data: null }) => {
+        return {
+          thenRespond: (callback: (event) => void) => {
+            callback(eventType);
+          },
+        };
+      });
+
+      applePayClientInitializer.register();
+      verify(applePayClientMock.init(anything())).once();
+    });
+  });
+});
