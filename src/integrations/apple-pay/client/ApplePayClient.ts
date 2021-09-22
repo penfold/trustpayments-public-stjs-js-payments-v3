e8e247e416c1e1082f0d86bb8744cf75e2749698
Piotr Dobrowolski <pdobrowolski99@gmail.com>
2021-09-22 09:29:46 +0200

    added update jwt function

--- src/integrations/apple-pay/client/ApplePayClient.ts
+++ src/integrations/apple-pay/client/ApplePayClient.ts
@@ -14,10 +14,16 @@ import { PUBLIC_EVENTS } from '../../../application/core/models/constants/EventT
 import { ApplePaySessionFactory } from '../../../client/integrations/apple-pay/apple-pay-session-service/ApplePaySessionFactory';
 import { ApplePayPaymentMethodName } from '../models/IApplePayPaymentMethod';
 import { MerchantValidationService } from './MerchantValidationService';
-import { mapTo, tap } from 'rxjs/operators';
+import { mapTo, takeUntil, tap } from 'rxjs/operators';
 import { IStartPaymentMethod } from '../../../application/core/services/payments/events/IStartPaymentMethod';
 import { PaymentAuthorizationService } from './PaymentAuthorizationService';
 import { IApplePayPaymentRequest } from '../../../application/core/integrations/apple-pay/apple-pay-payment-data/IApplePayPaymentRequest';
+import { ofType } from '../../../shared/services/message-bus/operators/ofType';
+import { IMessageBusEvent } from '../../../application/core/models/IMessageBusEvent';
+import { IUpdateJwt } from '../../../application/core/models/IUpdateJwt';
+import { IStJwtPayload } from '../../../application/core/models/IStJwtPayload';
+import { Money } from 'ts-money';
+import { JwtDecoder } from '../../../shared/services/jwt-decoder/JwtDecoder';
 
 @Service()
 export class ApplePayClient {
@@ -32,6 +38,7 @@ export class ApplePayClient {
     private messageBus: IMessageBus,
     private merchantValidationService: MerchantValidationService,
     private paymentAuthorizationService: PaymentAuthorizationService,
+    private jwtDecoder: JwtDecoder,
   ) {
   }
 
@@ -40,8 +47,37 @@ export class ApplePayClient {
       map(config => this.resolveApplePayConfig(config)),
       tap(applePayConfig => this.insertApplePayButton(applePayConfig)),
       tap(applePayConfig => this.initGestureHandler(applePayConfig)),
+      tap(() => this.updateJwtListener()),
       mapTo(undefined),
     );
+  }
+
+  private updateJwtListener(): void {
+    this.messageBus
+      .pipe(
+        ofType(PUBLIC_EVENTS.UPDATE_JWT),
+        tap((event: IMessageBusEvent<IUpdateJwt>) => {
+          this.updateConfigWithJWT(event.data.newJwt);
+        }),
+        takeUntil(this.messageBus.pipe(ofType(PUBLIC_EVENTS.DESTROY)))
+      )
+      .subscribe();
+  }
+
+  private updateConfigWithJWT(jwt: string): void {
+    console.log(jwt);
+    const { payload }: { payload: IStJwtPayload } = this.jwtDecoder.decode(jwt);
+
+    let totalPrice = payload.mainamount;
+
+    if (totalPrice === undefined) {
+      totalPrice = Money.fromInteger({
+        amount: parseInt(payload.baseamount, 10),
+        currency: payload.currencyiso3a,
+      }).toString();
+    }
+
+    console.log(totalPrice);
   }
 
   private isApplePayAvailable(config: IConfig): Observable<IConfig> {
