332b799c822e40fa375f69b2e95ea45e792e28d5
Grzegorz Martowski <gmartowski@pgs-soft.com>
2021-09-28 18:15:23 +0200

    STJS-2216 Prepare ZIP config schema - validation and schema

--- src/integrations/apm/models/APMName.ts
+++ src/integrations/apm/models/APMName.ts
@@ -2,13 +2,9 @@ export enum APMName {
   alipayinapp = 'alipayinapp', // Alipay In-App Payments
   americanexp = 'americanexp', // American Express
   bancontact = 'bancontact', //
-  diners = 'diners', // Diners Club International
-  discover = 'discover', // Discover Card
   epsuberweisung = 'epsuberweisung', // eps-uberweisung
   giropay = 'giropay', // giropay
   ideal = 'ideal', // iDEAL
-  jcb = 'jcb', // JCB
-  mastercard = 'mastercard', // Mastercard
   multibanco = 'multibanco', // Multibanco
   mybank = 'mybank', // MyBank
   paysafecard = 'paysafecard', // paysafecard
@@ -21,7 +17,6 @@ export enum APMName {
   sofort = 'sofort', // Sofort
   trustly = 'trustly', // Trustly
   unionpay = 'unionpay', // UnionPay SecurePay
-  visa = 'visa', // Visa
   wechat = 'wechat', // WeChat Pay
   // TODO: add some other payment names - to be discussed with the stakeholder
 }
