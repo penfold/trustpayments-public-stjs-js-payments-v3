c777eb3da36c7a14afb843cc6c845adfc013e16e
Grzegorz Martowski <gmartowski@pgs-soft.com>
2021-09-29 12:42:23 +0200

    STJS-2216 Delete full list and leave only zip

--- src/integrations/apm/models/APMName.ts
+++ src/integrations/apm/models/APMName.ts
@@ -1,23 +1,3 @@
 export enum APMName {
-  alipayinapp = 'alipayinapp', // Alipay In-App Payments
-  americanexp = 'americanexp', // American Express
-  bancontact = 'bancontact', //
-  epsuberweisung = 'epsuberweisung', // eps-uberweisung
-  giropay = 'giropay', // giropay
-  ideal = 'ideal', // iDEAL
-  multibanco = 'multibanco', // Multibanco
-  mybank = 'mybank', // MyBank
-  paysafecard = 'paysafecard', // paysafecard
-  payu = 'payu', // PayU
-  postfinance = 'postfinance', // PostFinance
-  przelewy24 = 'przelewy24', // Przelewy24
-  redpagos = 'redpagos', // redpagos
-  safetypay = 'safetypay', // SafetyPay
-  sepa = 'sepa', // SEPA Direct Debit
-  sofort = 'sofort', // Sofort
-  trustly = 'trustly', // Trustly
-  unionpay = 'unionpay', // UnionPay SecurePay
-  wechat = 'wechat', // WeChat Pay
-  zip = 'zip', // WeChat Pay
-  // TODO: a correct list will be provided by Ben
+  zip = 'zip',
 }
