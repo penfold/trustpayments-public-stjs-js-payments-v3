ae86c6fe0ad398ac962a5c6c954129cb89e7b436
Grzegorz Martowski <gmartowski@pgs-soft.com>
2021-09-30 14:52:32 +0200

    STJS-2217 Calling APM init client

--- src/client/st/ST.ts
+++ src/client/st/ST.ts
@@ -50,6 +50,9 @@ import { ITranslator } from '../../application/core/shared/translator/ITranslato
 import { IStJwtPayload } from '../../application/core/models/IStJwtPayload';
 import { ExposedEvents, ExposedEventsName } from '../../application/core/models/constants/ExposedEvents';
 import { EventScope } from '../../application/core/models/constants/EventScope';
+import { APMClient } from '../../integrations/apm/client/APMClient';
+import { IAPMConfig } from '../../integrations/apm/models/IAPMConfig';
+import { APMPaymentMethodName } from '../../integrations/apm/models/IAPMPaymentMethod';
 
 @Service()
 export class ST {
@@ -115,6 +118,7 @@ export class ST {
     private googleAnalytics: GoogleAnalytics,
     private merchantFields: MerchantFields,
     private cardFrames: CardFrames,
+    private apmClient: APMClient,
   ) {
   }
 
@@ -148,6 +152,21 @@ export class ST {
         {
           type: PUBLIC_EVENTS.CARD_PAYMENTS_INIT,
           data: JSON.stringify(this.config),
+        },
+        EventScope.THIS_FRAME,
+      );
+    });
+  }
+
+  APM(config: IAPMConfig): void {
+    this.initControlFrame$().subscribe(() => {
+      this.messageBus.publish<IInitPaymentMethod<IAPMConfig>>(
+        {
+          type: PUBLIC_EVENTS.APM_INIT_CLIENT,
+          data: {
+            name: APMPaymentMethodName,
+            config,
+          },
         },
         EventScope.THIS_FRAME,
       );
@@ -292,7 +311,7 @@ export class ST {
     this.messageBus.publish(
       {
         type: MessageBus.EVENTS_PUBLIC.THREED_CANCEL,
-      },  EventScope.ALL_FRAMES,
+      }, EventScope.ALL_FRAMES,
     );
   }
 
