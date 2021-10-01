87cbd7eee92e120c340988621c15e60244caab01
Wojciech Grabowski <wgrabowski@pgs-soft.com>
2021-10-01 08:50:00 +0000

    STJS-2242 Initial implementation of APMClient

--- src/integrations/apm/client/APMClient.ts
+++ src/integrations/apm/client/APMClient.ts
@@ -1,10 +1,34 @@
 import { Service } from 'typedi';
-import { IConfig } from '../../../shared/model/config/IConfig';
 import { Observable, of } from 'rxjs';
+import { DomMethods } from '../../../application/core/shared/dom-methods/DomMethods';
+import { IAPMItemConfig } from '../models/IAPMItemConfig';
+import { IAPMConfig } from '../models/IAPMConfig';
+import { APMConfigResolver } from '../services/apm-config-resolver/APMConfigResolver';
 
 @Service()
 export class APMClient {
-  init(config: IConfig): Observable<unknown> {
-    return of(null); // TODO
+  constructor(private apmUtils: APMConfigResolver) {
+  }
+
+  init(config: IAPMConfig): Observable<undefined> {
+    this.apmUtils.resolve(config).apmList.forEach(itemConfig => this.insertAPMButton(itemConfig as IAPMItemConfig));
+
+    return of(undefined);
+  }
+
+  private insertAPMButton(apmItemConfig: IAPMItemConfig) {
+    DomMethods.appendChildStrictIntoDOM(apmItemConfig.placement, this.createButtonForApmItem(apmItemConfig));
+  }
+
+  private createButtonForApmItem(apmItemConfig: IAPMItemConfig): HTMLElement {
+    const button = DomMethods.createHtmlElement({ type: 'button' }, 'button');
+    button.addEventListener('click', (event) => this.onAPMButtonClick(event, apmItemConfig));
+
+    return button;
+  }
+
+  private onAPMButtonClick(event: Event, { name }: IAPMItemConfig) {
+    event.preventDefault();
+    console.log(`${name} payment button clicked`); // TODO start payement here
   }
 }
