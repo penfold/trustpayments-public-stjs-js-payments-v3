325026d8130d5280dca8483f3186457ebf300428
Grzegorz Martowski <gmartowski@pgs-soft.com>
2021-10-21 12:51:11 +0200

    STJS-2443 APM list filtering

--- src/integrations/apm/models/APMAvailabilityMap.ts
+++ src/integrations/apm/models/APMAvailabilityMap.ts
@@ -1,7 +1,93 @@
 import { APMName } from './APMName';
 
 export const APMAvailabilityMap: Map<APMName, { countries: string[], currencies: string[] }> = new Map()
+  .set('ALIPAY' as APMName, {
+    countries: ['AUD', 'CAD', 'CHF', 'DKK', 'EUR', 'en_GB', 'HKD', 'JPY', 'KRW', 'NOK', 'NZD', 'SEK', 'SGD', 'THB', 'USD'],
+    currencies: ['EUR', 'GBP', 'USD'],
+  })
+  .set('BANCONTACT' as APMName, {
+    countries: ['AUD', 'CAD', 'CHF', 'DKK', 'EUR', 'en_GB', 'HKD', 'JPY', 'KRW', 'NOK', 'NZD', 'SEK', 'SGD', 'THB', 'USD'],
+    currencies: ['EUR', 'GBP', 'USD'],
+  })
+  .set('EPS' as APMName, {
+    countries: ['AUD', 'CAD', 'CHF', 'DKK', 'EUR', 'en_GB', 'HKD', 'JPY', 'KRW', 'NOK', 'NZD', 'SEK', 'SGD', 'THB', 'USD'],
+    currencies: ['EUR', 'GBP', 'USD'],
+  })
+  .set('GIROPAY' as APMName, {
+    countries: ['AUD', 'CAD', 'CHF', 'DKK', 'EUR', 'en_GB', 'HKD', 'JPY', 'KRW', 'NOK', 'NZD', 'SEK', 'SGD', 'THB', 'USD'],
+    currencies: ['EUR', 'GBP', 'USD'],
+  })
+  .set('IDEAL' as APMName, {
+    countries: ['AUD', 'CAD', 'CHF', 'DKK', 'EUR', 'en_GB', 'HKD', 'JPY', 'KRW', 'NOK', 'NZD', 'SEK', 'SGD', 'THB', 'USD'],
+    currencies: ['EUR', 'GBP', 'USD'],
+  })
+  .set('MULTIBANCO' as APMName, {
+    countries: ['AUD', 'CAD', 'CHF', 'DKK', 'EUR', 'en_GB', 'HKD', 'JPY', 'KRW', 'NOK', 'NZD', 'SEK', 'SGD', 'THB', 'USD'],
+    currencies: ['EUR', 'GBP', 'USD'],
+  })
+  .set('MYBANK' as APMName, {
+    countries: ['AUD', 'CAD', 'CHF', 'DKK', 'EUR', 'en_GB', 'HKD', 'JPY', 'KRW', 'NOK', 'NZD', 'SEK', 'SGD', 'THB', 'USD'],
+    currencies: ['EUR', 'GBP', 'USD'],
+  })
+  .set('PAYPAL' as APMName, {
+    countries: ['AUD', 'CAD', 'CHF', 'DKK', 'EUR', 'en_GB', 'HKD', 'JPY', 'KRW', 'NOK', 'NZD', 'SEK', 'SGD', 'THB', 'USD'],
+    currencies: ['EUR', 'GBP', 'USD'],
+  })
+  .set('PAYSAFECARD' as APMName, {
+    countries: ['AUD', 'CAD', 'CHF', 'DKK', 'EUR', 'en_GB', 'HKD', 'JPY', 'KRW', 'NOK', 'NZD', 'SEK', 'SGD', 'THB', 'USD'],
+    currencies: ['EUR', 'GBP', 'USD'],
+  })
+  .set('PAYU' as APMName, {
+    countries: ['AUD', 'CAD', 'CHF', 'DKK', 'EUR', 'en_GB', 'HKD', 'JPY', 'KRW', 'NOK', 'NZD', 'SEK', 'SGD', 'THB', 'USD'],
+    currencies: ['EUR', 'GBP', 'USD'],
+  })
+  .set('POSTFINANCE' as APMName, {
+    countries: ['AUD', 'CAD', 'CHF', 'DKK', 'EUR', 'en_GB', 'HKD', 'JPY', 'KRW', 'NOK', 'NZD', 'SEK', 'SGD', 'THB', 'USD'],
+    currencies: ['EUR', 'GBP', 'USD'],
+  })
+  .set('PRZELEWY24' as APMName, {
+    countries: ['AUD', 'CAD', 'CHF', 'DKK', 'EUR', 'en_GB', 'HKD', 'JPY', 'KRW', 'NOK', 'NZD', 'SEK', 'SGD', 'THB', 'USD'],
+    currencies: ['EUR', 'GBP', 'USD'],
+  })
+  .set('QIWI' as APMName, {
+    countries: ['AUD', 'CAD', 'CHF', 'DKK', 'EUR', 'en_GB', 'HKD', 'JPY', 'KRW', 'NOK', 'NZD', 'SEK', 'SGD', 'THB', 'USD'],
+    currencies: ['EUR', 'GBP', 'USD'],
+  })
+  .set('REDPAGOS' as APMName, {
+    countries: ['AUD', 'CAD', 'CHF', 'DKK', 'EUR', 'en_GB', 'HKD', 'JPY', 'KRW', 'NOK', 'NZD', 'SEK', 'SGD', 'THB', 'USD'],
+    currencies: ['EUR', 'GBP', 'USD'],
+  })
+  .set('SAFETYPAY' as APMName, {
+    countries: ['AUD', 'CAD', 'CHF', 'DKK', 'EUR', 'en_GB', 'HKD', 'JPY', 'KRW', 'NOK', 'NZD', 'SEK', 'SGD', 'THB', 'USD'],
+    currencies: ['EUR', 'GBP', 'USD'],
+  })
+  .set('SEPADD' as APMName, {
+    countries: ['AUD', 'CAD', 'CHF', 'DKK', 'EUR', 'en_GB', 'HKD', 'JPY', 'KRW', 'NOK', 'NZD', 'SEK', 'SGD', 'THB', 'USD'],
+    currencies: ['EUR', 'GBP', 'USD'],
+  })
+  .set('SOFORT' as APMName, {
+    countries: ['AUD', 'CAD', 'CHF', 'DKK', 'EUR', 'en_GB', 'HKD', 'JPY', 'KRW', 'NOK', 'NZD', 'SEK', 'SGD', 'THB', 'USD'],
+    currencies: ['EUR', 'GBP', 'USD'],
+  })
+  .set('TRUSTLY' as APMName, {
+    countries: ['AUD', 'CAD', 'CHF', 'DKK', 'EUR', 'en_GB', 'HKD', 'JPY', 'KRW', 'NOK', 'NZD', 'SEK', 'SGD', 'THB', 'USD'],
+    currencies: ['EUR', 'GBP', 'USD'],
+  })
+  .set('UNIONPAY' as APMName, {
+    countries: ['AUD', 'CAD', 'CHF', 'DKK', 'EUR', 'en_GB', 'HKD', 'JPY', 'KRW', 'NOK', 'NZD', 'SEK', 'SGD', 'THB', 'USD'],
+    currencies: ['EUR', 'GBP', 'USD'],
+  })
+  .set('WECHATPAY' as APMName, {
+    countries: ['AUD', 'CAD', 'CHF', 'DKK', 'EUR', 'en_GB', 'HKD', 'JPY', 'KRW', 'NOK', 'NZD', 'SEK', 'SGD', 'THB', 'USD'],
+    currencies: ['EUR', 'GBP', 'USD'],
+  })
   .set(APMName.ZIP, {
     countries: ['AUD', 'CAD', 'CHF', 'DKK', 'EUR', 'en_GB', 'HKD', 'JPY', 'KRW', 'NOK', 'NZD', 'SEK', 'SGD', 'THB', 'USD'],
     currencies: ['EUR', 'GBP', 'USD'],
   });
+
+
+
+
+
+
---
+++ src/integrations/apm/services/apm-locale-transform/APMLocaleTransform.ts
@@ -0,0 +1,8 @@
+const localeCurrencyMap = new Map()
+  .set('en_GB', 'EN')
+
+export class APMLocaleTransform {
+  toUpperCase(locale: string) {
+
+  }
+}
