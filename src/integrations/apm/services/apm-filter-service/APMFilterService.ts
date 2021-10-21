69ec8239c9d74633e4de3c3c384d06f2d5334933
Grzegorz Martowski <gmartowski@pgs-soft.com>
2021-10-21 13:31:07 +0200

    STJS-2443 APM list filtering

--- src/integrations/apm/services/apm-filter-service/APMFilterService.ts
+++ src/integrations/apm/services/apm-filter-service/APMFilterService.ts
@@ -1,8 +1,7 @@
-import { EMPTY, Observable, of, switchMap } from 'rxjs';
+import { Observable, of } from 'rxjs';
 import { IAPMItemConfig } from '../../models/IAPMItemConfig';
 import { JwtDecoder } from '../../../../shared/services/jwt-decoder/JwtDecoder';
 import { ConfigProvider } from '../../../../shared/services/config-provider/ConfigProvider';
-import { IConfig } from '../../../../shared/model/config/IConfig';
 import { IStJwtPayload } from '../../../../application/core/models/IStJwtPayload';
 import { APMAvailabilityMap } from '../../models/APMAvailabilityMap';
 import { Service } from 'typedi';
@@ -12,31 +11,30 @@ import { IUpdateJwt } from '../../../../application/core/models/IUpdateJwt';
 
 @Service()
 export class APMFilterService {
-  private currency: string;
-  private country: string;
-
   constructor(private jwtDecoder: JwtDecoder, private configProvider: ConfigProvider, private messageBus: IMessageBus) {
     this.messageBus.subscribeType(PUBLIC_EVENTS.UPDATE_JWT, (data: IUpdateJwt) => {
-
+      const { country, currency } = this.getCurrencyAndCountry(data.newJwt);
+      this.messageBus.publish({ data: { country, currency }, type: PUBLIC_EVENTS.APM_CURRENCY_AND_COUNTRY });
     });
-    this.init$();
-  }
-
-  private init$(): Observable<any> {
-    return this.configProvider.getConfig$().pipe(
-      switchMap((config: IConfig) => {
-        const { currencyiso3a, locale } = this.jwtDecoder.decode<IStJwtPayload>(config.jwt).payload;
-        return EMPTY;
-      }),
-    );
   }
 
   filter(apmList: IAPMItemConfig[]): Observable<IAPMItemConfig[]> {
-    const { currencyiso3a, locale } = this.jwtDecoder.decode<IStJwtPayload>(this.configProvider.getConfig().jwt).payload;
-    return of(apmList.filter((item: IAPMItemConfig) => {
-      return APMAvailabilityMap.has(item.name) &&
-        APMAvailabilityMap.get(item.name).currencies.includes(currencyiso3a) &&
-        APMAvailabilityMap.get(item.name).countries.includes(locale)
-    }));
+    const { country, currency } = this.getCurrencyAndCountry(this.configProvider.getConfig().jwt);
+
+    return of(apmList.filter((item: IAPMItemConfig) => this.isAPMAvailable(item, currency, country)));
+  }
+
+  private getCurrencyAndCountry(jwt: string): { currency: string, country: string } {
+
+    return {
+      currency: this.jwtDecoder.decode<IStJwtPayload>(jwt).payload.currencyiso3a,
+      country: this.jwtDecoder.decode<IStJwtPayload>(jwt).payload.locale,
+    };
+  }
+
+  private isAPMAvailable(item: IAPMItemConfig, currencyiso3a: string, locale: string): boolean {
+    return APMAvailabilityMap.has(item.name) &&
+      APMAvailabilityMap.get(item.name).currencies.includes(currencyiso3a) &&
+      APMAvailabilityMap.get(item.name).countries.includes(locale);
   }
 }
