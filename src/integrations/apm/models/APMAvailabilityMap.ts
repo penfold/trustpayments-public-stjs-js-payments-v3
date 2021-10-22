60f2beaf33b0eeb2e5a9c7054c777f36e9f01193
Grzegorz Martowski <gmartowski@pgs-soft.com>
2021-10-22 09:00:15 +0200

    STJS-2443 Country and currency iso list

--- src/integrations/apm/models/APMAvailabilityMap.ts
+++ src/integrations/apm/models/APMAvailabilityMap.ts
@@ -1,93 +1,89 @@
 import { APMName } from './APMName';
+import { APMCountryIso } from './APMCountryIso';
+import { APMCurrencyIso } from './APMCurrencyIso';
 
 export const APMAvailabilityMap: Map<APMName, { countries: string[], currencies: string[] }> = new Map()
   .set('ALIPAY' as APMName, {
     countries: [],
-    currencies: ['EUR', 'GBP', 'USD'],
+    currencies: [APMCurrencyIso.EUR, APMCurrencyIso.GBP, APMCurrencyIso.USD],
   })
   .set('BANCONTACT' as APMName, {
-    countries: ['BE'],
-    currencies: ['EUR'],
+    countries: [APMCountryIso.BE],
+    currencies: [APMCurrencyIso.EUR],
   })
   .set('BITPAY' as APMName, {
     countries: [],
-    currencies: ['EUR', 'GBP', 'USD'],
+    currencies: [APMCurrencyIso.EUR, APMCurrencyIso.GBP, APMCurrencyIso.USD],
   })
   .set('EPS' as APMName, {
-    countries: ['AT'],
-    currencies: ['EUR'],
+    countries: [APMCountryIso.AT],
+    currencies: [APMCurrencyIso.EUR],
   })
   .set('GIROPAY' as APMName, {
-    countries: ['DE'],
-    currencies: ['EUR'],
+    countries: [APMCountryIso.DE],
+    currencies: [APMCurrencyIso.EUR],
   })
   .set('IDEAL' as APMName, {
-    countries: ['NL'],
-    currencies: ['EUR'],
+    countries: [APMCountryIso.NL],
+    currencies: [APMCurrencyIso.EUR],
   })
   .set('MULTIBANCO' as APMName, {
-    countries: ['PT'],
-    currencies: ['EUR'],
+    countries: [APMCountryIso.PT],
+    currencies: [APMCurrencyIso.EUR],
   })
   .set('MYBANK' as APMName, {
-    countries: ['IT'],
-    currencies: ['EUR'],
+    countries: [APMCountryIso.IT],
+    currencies: [APMCurrencyIso.EUR],
   })
   .set('PAYPAL' as APMName, {
     countries: [],
-    currencies: ['AUD', 'CAD', 'EUR', 'GBP', 'JPY', 'USD'],
+    currencies: [APMCurrencyIso.AUD, APMCurrencyIso.CAD, APMCurrencyIso.EUR, APMCurrencyIso.GBP, 'JPY', APMCurrencyIso.USD],
   })
   .set('PAYSAFECARD' as APMName, {
     countries: [],
-    currencies: ['ARS', 'AUD', 'BGN', 'CAD', 'CHF', 'CZK', 'DKK', 'EUR', 'GBP', 'HRK', 'HUF', 'MXN', 'NOK', 'NZD', 'PEN', 'PLN', 'RON', 'SEK', 'TRY', 'USD', 'UYU'],
+    currencies: [APMCurrencyIso.ARS, APMCurrencyIso.AUD, APMCurrencyIso.BGN, APMCurrencyIso.CAD, APMCurrencyIso.CHF, APMCurrencyIso, APMCurrencyIso.DKK, APMCurrencyIso.EUR, APMCurrencyIso.GBP, APMCurrencyIso.HRK, APMCurrencyIso.HUF, APMCurrencyIso.MXN, APMCurrencyIso.NOK, APMCurrencyIso.NZD, APMCurrencyIso.PEN, APMCurrencyIso.PLN, APMCurrencyIso.RON, APMCurrencyIso.SEK, APMCurrencyIso.TRY, APMCurrencyIso.USD, APMCurrencyIso.UYU],
   })
   .set('PAYU' as APMName, {
-    countries: ['CZ', 'PL'],
-    currencies: ['CZK', 'PLN'],
+    countries: [APMCountryIso.CZ, APMCountryIso.PL],
+    currencies: [APMCurrencyIso.CZK, APMCurrencyIso.PLN],
   })
   .set('POSTFINANCE' as APMName, {
-    countries: ['CH'],
-    currencies: ['CHF', 'EUR'],
+    countries: [APMCountryIso.CH],
+    currencies: [APMCurrencyIso.CHF, APMCurrencyIso.EUR],
   })
   .set('PRZELEWY24' as APMName, {
-    countries: ['PL'],
-    currencies: ['EUR', 'PLN'],
+    countries: [APMCountryIso.PL],
+    currencies: [APMCurrencyIso.EUR, APMCurrencyIso.PLN],
   })
   .set('REDPAGOS' as APMName, {
-    countries: ['UY'],
-    currencies: ['USD'],
+    countries: [APMCountryIso.UY],
+    currencies: [APMCurrencyIso.USD],
   })
   .set('SAFETYPAY' as APMName, {
-    countries: ['AT', 'BE', 'CL', 'CO', 'CR', 'DE', 'EC', 'ES', 'MX', 'NL', 'PE', 'PR'],
-    currencies: ['EUR', 'USD'],
+    countries: [APMCountryIso.AT, APMCountryIso.BE, APMCountryIso.CL, APMCountryIso.CO, APMCountryIso.CR, APMCountryIso.DE, APMCountryIso.EC, APMCountryIso.ES, APMCountryIso.MX, APMCountryIso.NL, APMCountryIso.PE, APMCountryIso.PR],
+    currencies: [APMCurrencyIso.EUR, APMCurrencyIso.USD],
   })
   .set('SEPADD' as APMName, {
-    countries: ['AT', 'BE', 'CY', 'DE', 'EE', 'ES', 'FI', 'FR', 'GR', 'IE', 'IT', 'LT', 'LU', 'LV', 'MC', 'MT', 'NL', 'PT', 'SI', 'SK'],
-    currencies: ['EUR'],
+    countries: [APMCountryIso.AT, APMCountryIso.BE, APMCountryIso.CY, APMCountryIso.DE, APMCountryIso.EE, APMCountryIso.ES, APMCountryIso.FI, APMCountryIso.FR, APMCountryIso.GR, APMCountryIso.IE, APMCountryIso.IT, APMCountryIso.LT, APMCountryIso.LU, APMCountryIso.LV, APMCountryIso.MC, APMCountryIso.MT, APMCountryIso.NL, APMCountryIso.PT, APMCountryIso.SI, APMCountryIso.SK],
+    currencies: [APMCurrencyIso.EUR],
   })
   .set('SOFORT' as APMName, {
-    countries: ['AT', 'BE', 'CH', 'DE', 'ES', 'IT', 'NL', 'PL'],
-    currencies: ['EUR'],
+    countries: [APMCountryIso.AT, APMCountryIso.BE, APMCountryIso.CH, APMCountryIso.DE, APMCountryIso.ES, APMCountryIso.IT, APMCountryIso.NL, APMCountryIso.PL],
+    currencies: [APMCurrencyIso.EUR],
   })
   .set('TRUSTLY' as APMName, {
-    countries: ['DK', 'EE', 'ES', 'FI', 'IT', 'NO', 'PL', 'SE'],
-    currencies: ['DKK', 'EUR', 'NOK', 'PLN', 'SEK'],
+    countries: [APMCountryIso.DK, APMCountryIso.EE, APMCountryIso.ES, APMCountryIso.FI, APMCountryIso.IT, APMCountryIso.NO, APMCountryIso.PL, APMCountryIso.SE],
+    currencies: ['DKK', APMCurrencyIso.EUR, APMCurrencyIso.NOK, APMCurrencyIso.PLN, APMCurrencyIso.SEK],
   })
   .set('UNIONPAY' as APMName, {
     countries: [],
-    currencies: ['AUD', 'CAD', 'CHF', 'CNY', 'EUR', 'GBP', 'HKD', 'JPY', 'NZD', 'SGD', 'USD'],
+    currencies: [APMCurrencyIso.AUD, APMCurrencyIso.CAD, APMCurrencyIso.CHF, APMCurrencyIso.CNY, APMCurrencyIso.EUR, APMCurrencyIso.GBP, APMCurrencyIso.HKD, APMCurrencyIso.JPY, APMCurrencyIso.NZD, APMCurrencyIso.SGD, APMCurrencyIso.USD],
   })
   .set('WECHATPAY' as APMName, {
-    countries: ['CN'],
-    currencies: ['EUR', 'USD'],
+    countries: [APMCountryIso.CN],
+    currencies: [APMCurrencyIso.EUR, APMCurrencyIso.USD],
   })
   .set(APMName.ZIP, {
-    countries: ['AT', 'GB', 'NZ'],
-    currencies: ['AUD', 'GBP', 'NZD'],
+    countries: [APMCountryIso.AT, APMCountryIso.GB, APMCountryIso.NZ],
+    currencies: [APMCurrencyIso.AUD, APMCurrencyIso.GBP, APMCurrencyIso.NZD],
   });
-
-
-
-
-
-
