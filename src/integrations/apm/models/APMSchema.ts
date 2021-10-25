a86fddfb82dd19f5fc64ae27f22b092482f8df5c
Grzegorz Martowski <gmartowski@pgs-soft.com>
2021-10-25 06:36:38 +0000

    STJS-2281 PPRO APMs - UnionPay Secure Pay

--- src/integrations/apm/models/APMSchema.ts
+++ src/integrations/apm/models/APMSchema.ts
@@ -26,4 +26,5 @@ export const APMSchemasMap: Map<APMName, ObjectSchema> = new Map()
   .set(APMName.PAYU, configSchemaFactory(APMName.PAYU))
   .set(APMName.POSTFINANCE, configSchemaFactory(APMName.POSTFINANCE))
   .set(APMName.PRZELEWY24, configSchemaFactory(APMName.PRZELEWY24))
+  .set(APMName.UNIONPAY, configSchemaFactory(APMName.UNIONPAY))
   .set(APMName.ZIP, configSchemaFactory(APMName.ZIP));
