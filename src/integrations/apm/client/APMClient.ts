5e8204a8c54d7b986a63c6e7b8a2a663f706ac74
Grzegorz Martowski <gmartowski@pgs-soft.com>
2021-10-25 08:49:21 +0000

    STJS-2294: PPRO APMs - Giropay

--- src/integrations/apm/client/APMClient.ts
+++ src/integrations/apm/client/APMClient.ts
@@ -19,6 +19,7 @@ import './APMClient.scss';
 export class APMClient {
   private apmIcons: Record<APMName, string> = {
     [APMName.BITPAY]: require('./images/bitpay.svg'),
+    [APMName.GIROPAY]: require('./images/giropay.svg'),
     [APMName.EPS]: require('./images/eps.svg'),
     [APMName.IDEAL]: require('./images/ideal.svg'),
     [APMName.MYBANK]: require('./images/mybank.svg'),
--- src/integrations/apm/models/APMName.ts
+++ src/integrations/apm/models/APMName.ts
@@ -1,5 +1,6 @@
 export enum APMName {
   BITPAY = 'BITPAY',
+  GIROPAY = 'GIROPAY',
   EPS = 'EPS',
   IDEAL = 'IDEAL',
   MYBANK = 'MYBANK',
