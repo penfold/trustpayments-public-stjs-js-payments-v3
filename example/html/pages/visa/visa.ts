4125595b9611a68ffef556711cc1dec71d966670
Maciej Krawczyk <mackrawczyk@pgs-soft.com>
2022-01-11 12:33:30 +0100

    added console log before email prompt

--- example/html/pages/visa/visa.ts
+++ example/html/pages/visa/visa.ts
@@ -33,6 +33,8 @@ window.addEventListener('load', async () => {
   }
 
   async function identityLookup() {
+    console.log('awaiting email');
+    
     const email = prompt('Email address', '');
 
     if (!email?.length) {
