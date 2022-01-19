62c3449739a178d88a9e49a3c5138158f0a1a36e
Wojciech Grabowski <wgrabowski@pgs-soft.com>
2022-01-19 08:11:49 +0000

    STJS-2935 Initialize request processing service before initializing click to pay payment method

--- src/integrations/click-to-pay/application/ClickToPayPaymentMethod.spec.ts
+++ src/integrations/click-to-pay/application/ClickToPayPaymentMethod.spec.ts
@@ -1,29 +1,37 @@
-import { anything, instance, mock, objectContaining, verify, when } from 'ts-mockito';
+import { instance, mock, objectContaining, verify, when } from 'ts-mockito';
 import { of } from 'rxjs';
+import { mapTo } from 'rxjs/operators';
 import { IFrameQueryingService } from '../../../shared/services/message-bus/interfaces/IFrameQueryingService';
 import { IClickToPayConfig } from '../models/IClickToPayConfig';
 import { IMessageBusEvent } from '../../../application/core/models/IMessageBusEvent';
 import { PUBLIC_EVENTS } from '../../../application/core/models/constants/EventTypes';
 import { MERCHANT_PARENT_FRAME } from '../../../application/core/models/constants/Selectors';
+import { RequestProcessingInitializer } from '../../../application/core/services/request-processor/RequestProcessingInitializer';
+import { IRequestProcessingService } from '../../../application/core/services/request-processor/IRequestProcessingService';
 import { ClickToPayPaymentMethod } from './ClickToPayPaymentMethod';
 
 describe('ClickToPayPaymentMethod', () => {
   let sut: ClickToPayPaymentMethod;
   let frameQueryingService: IFrameQueryingService;
+  let requestProcessingInitializerMock: RequestProcessingInitializer;
+  let requestProcessingServiceMock: IRequestProcessingService;
+  const testConfig: IClickToPayConfig = {};
+  const expectedEventObject: IMessageBusEvent<IClickToPayConfig> = {
+    type: PUBLIC_EVENTS.CLICK_TO_PAY_INIT_CLIENT,
+    data: testConfig,
+  };
 
   beforeEach(() => {
     frameQueryingService = mock<IFrameQueryingService>();
-    when(frameQueryingService.query(anything(), anything())).thenReturn(of(null));
-    sut = new ClickToPayPaymentMethod(instance(frameQueryingService));
+    requestProcessingInitializerMock = mock(RequestProcessingInitializer);
+    requestProcessingServiceMock = mock<IRequestProcessingService>();
+    when(requestProcessingInitializerMock.initialize()).thenReturn(of(null).pipe(mapTo(instance(requestProcessingServiceMock))));
+    when(frameQueryingService.query(objectContaining(expectedEventObject), MERCHANT_PARENT_FRAME)).thenReturn(of(undefined));
+    sut = new ClickToPayPaymentMethod(instance(frameQueryingService), instance(requestProcessingInitializerMock));
   });
 
   describe('init()', () => {
     it('should should publish ClickToPay payment method client initialization event via frame querying service', done => {
-      const testConfig: IClickToPayConfig = {};
-      const expectedEventObject: IMessageBusEvent<IClickToPayConfig> = {
-        type: PUBLIC_EVENTS.CLICK_TO_PAY_INIT_CLIENT,
-        data: testConfig,
-      };
       sut.init(testConfig).subscribe(() => {
         verify(frameQueryingService.query(objectContaining(expectedEventObject), MERCHANT_PARENT_FRAME)).once();
         done();
--- src/integrations/click-to-pay/application/ClickToPayPaymentMethod.ts
+++ src/integrations/click-to-pay/application/ClickToPayPaymentMethod.ts
@@ -1,5 +1,6 @@
-import { Observable } from 'rxjs';
+import { forkJoin, Observable } from 'rxjs';
 import { Service } from 'typedi';
+import { mapTo } from 'rxjs/operators';
 import { IPaymentMethod } from '../../../application/core/services/payments/IPaymentMethod';
 import { IPaymentResult } from '../../../application/core/services/payments/IPaymentResult';
 import { ClickToPayPaymentMethodName } from '../models/ClickToPayPaymentMethodName';
@@ -9,10 +10,15 @@ import { PUBLIC_EVENTS } from '../../../application/core/models/constants/EventT
 import { IFrameQueryingService } from '../../../shared/services/message-bus/interfaces/IFrameQueryingService';
 import { MERCHANT_PARENT_FRAME } from '../../../application/core/models/constants/Selectors';
 import { PaymentMethodToken } from '../../../application/dependency-injection/InjectionTokens';
+import { RequestProcessingInitializer } from '../../../application/core/services/request-processor/RequestProcessingInitializer';
+import { IRequestProcessingService } from '../../../application/core/services/request-processor/IRequestProcessingService';
 
 @Service({ id: PaymentMethodToken, multiple: true })
 export class ClickToPayPaymentMethod implements IPaymentMethod<IClickToPayConfig> {
-  constructor(private frameQueryingService: IFrameQueryingService) {
+  private requestProcessingService: Observable<IRequestProcessingService>;
+
+  constructor(private frameQueryingService: IFrameQueryingService,
+              private requestProcessingInitializer: RequestProcessingInitializer) {
   }
 
   getName(): string {
@@ -20,11 +26,17 @@ export class ClickToPayPaymentMethod implements IPaymentMethod<IClickToPayConfig
   }
 
   init(config: IClickToPayConfig): Observable<undefined> {
+    this.requestProcessingService = this.requestProcessingInitializer.initialize();
+
     const clientInitEvent: IMessageBusEvent<IClickToPayConfig> = {
       type: PUBLIC_EVENTS.CLICK_TO_PAY_INIT_CLIENT,
       data: config,
     };
-    return this.frameQueryingService.query(clientInitEvent, MERCHANT_PARENT_FRAME);
+
+    return forkJoin([
+      this.requestProcessingService,
+      this.frameQueryingService.query(clientInitEvent, MERCHANT_PARENT_FRAME),
+    ]).pipe(mapTo(undefined));
   }
 
   start(data: unknown): Observable<IPaymentResult<unknown>> {
