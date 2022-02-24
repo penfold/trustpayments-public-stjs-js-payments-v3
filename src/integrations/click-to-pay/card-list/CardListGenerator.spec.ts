a4ef03db09bfdf5c38b0176238bed5ec69d20318
Lukasz Szwedo (PGS Software) <lszwedo@pgs-soft.com>
2022-02-24 13:45:15 +0100

    STJS-2979: added form to add new card

--- src/integrations/click-to-pay/card-list/CardListGenerator.spec.ts
+++ src/integrations/click-to-pay/card-list/CardListGenerator.spec.ts
@@ -1,10 +1,9 @@
 import { instance as mockInstance, mock, when } from 'ts-mockito';
 import { IconMap, mapIcon } from '../../../application/core/services/icon/IconMap';
-import { CardListGenerator } from './card-list-generator';
+import { CardListGenerator } from './CardListGenerator';
 import { cardListMock } from './card-list-mock';
 
 describe('CardListGenerator', () => {
-
   const iconMap: IconMap = mock(IconMap);
   Object.keys(mapIcon).map(item => {
     when(iconMap.getUrl(item)).thenReturn(mapIcon[item]);
@@ -18,20 +17,20 @@ describe('CardListGenerator', () => {
 
   it('generates html for single active card', () => {
     const expected = `
-      <span class="cards__card__checkbox">
-        <span style='display: none'><input type="radio" name="card" value="001" id="radio001"></span><img name="tick" id="tick001" src="images/empty.png" alt="">
+      <span class="st-card__checkbox">
+        <span style='display: none'><input type="radio" name="srcDigitalCardId" value="001" id="radio001"></span><span class="st-card__tick" id="tick001" name="tick"></span>
       </span>
-      <span class="cards__card__image">
+      <span class="st-card__image">
         <img src="card001Url" alt="" style="width: 60px; height: 40px">
       </span>
-      <span class="cards__card__description">
+      <span class="st-card__description">
         VISA<br>
         ..6263
       </span>
-      <span class="cards__card__logo">
+      <span class="st-card__logo">
         <img src="images/click-to-pay.png" alt="">
       </span>
-      <span class="cards__card__type">
+      <span class="st-card__type">
         <img src="./images/visa.png" alt="">
       </span>
     `;
@@ -43,20 +42,20 @@ describe('CardListGenerator', () => {
 
   it('generates html for single inactive card', () => {
     const expected = `
-      <span class="cards__card__checkbox">
+      <span class="st-card__checkbox">
         <span style='display: none'></span>
       </span>
-      <span class="cards__card__image">
+      <span class="st-card__image">
         <img src="card002Url" alt="" style="width: 60px; height: 40px">
       </span>
-      <span class="cards__card__description">
+      <span class="st-card__description">
         VISA<br>
         ..1345
       </span>
-      <span class="cards__card__logo">
+      <span class="st-card__logo">
         <img src="images/click-to-pay.png" alt="">
       </span>
-      <span class="cards__card__type">
+      <span class="st-card__type">
         <img src="./images/visa.png" alt="">
       </span>
     `;
