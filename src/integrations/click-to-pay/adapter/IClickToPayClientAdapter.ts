21611155915065651ad55160b1f87b9395f1d74e
Maciej Krawczyk <mackrawczyk@pgs-soft.com>
2022-02-24 12:49:00 +0100

    updates after review

--- src/integrations/click-to-pay/adapter/IClickToPayClientAdapter.ts
+++ src/integrations/click-to-pay/adapter/IClickToPayClientAdapter.ts
@@ -4,6 +4,5 @@ export interface IClickToPayAdapter<P extends IClickToPayAdapterInitParams, C> {
   init(initParams: P): Promise<C>;
   isRecognized(): Promise<boolean>;
   identifyUser(): Promise<boolean>;
-  showCardList(): Promise<void>;
 }
 
