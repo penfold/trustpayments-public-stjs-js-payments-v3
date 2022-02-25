20dddc469bfda8aea1ec4aaf9eeb08780c3437b3
Marek Sakowicz (PGS Software) <msakowicz@pgs-soft.com>
2022-02-25 11:32:16 +0100

    added steps implementation for receiving otp from gmail api

--- tests/features/steps/visa_ctp_steps.py
+++ tests/features/steps/visa_ctp_steps.py
@@ -1,8 +1,10 @@
 # type: ignore[no-redef]
+
 from behave import use_step_matcher, step, then
 
 from pages.page_factory import Pages
 from utils.enums.card import Card
+from utils.helpers.gmail_service import EMAIL_LOGIN
 
 use_step_matcher('re')
 
@@ -13,15 +15,23 @@ def step_impl(context):
     vctp_page.fill_billing_details_form()
 
 
+@step('User fills delivery details fields')
+def step_impl(context):
+    vctp_page = context.page_factory.get_page(Pages.VISA_CTP_PAGE)
+    vctp_page.fill_delivery_details_form()
+
+
 @step("User fills VISA_CTP card details with defined card (?P<card>.+)")
 def step_impl(context, card: Card):
-    payment_page = context.page_factory.get_page(Pages.PAYMENT_METHODS_PAGE)
+    vctp_page = context.page_factory.get_page(Pages.VISA_CTP_PAGE)
     card = Card.__members__[card]  # pylint: disable=unsubscriptable-object
-    context.pan = str(card.number)
-    context.exp_date = str(card.expiration_date)
-    context.cvv = str(card.cvv)
-    payment_page.fill_payment_form(card.number, card.expiration_date, card.cvv)
-    raise NotImplementedError(u'User fills VISA_CTP card details with defined card:')
+    vctp_page.fill_payment_form(card.number, card.expiration_date, card.cvv)
+
+
+@step('User clicks on "Look up my cards" link')
+def step_impl(context):
+    vctp_page = context.page_factory.get_page(Pages.VISA_CTP_PAGE)
+    vctp_page.click_look_up_my_cards_btn()
 
 
 @step('User reviews VISA_CTP checkout page')
@@ -29,9 +39,17 @@ def step_impl(context):
     raise NotImplementedError(u'STEP: And User reviews check-out page <condition> registering as a new user')
 
 
+@step('User login to VISA_CTP account with valid e-mail address')
+def step_impl(context):
+    vctp_page = context.page_factory.get_page(Pages.VISA_CTP_PAGE)
+    vctp_page.fill_email_input(EMAIL_LOGIN)
+    vctp_page.click_submit_email_btn()
+
+
 @step('User fills VISA_CTP one time password')
 def step_impl(context):
-    raise NotImplementedError(u'STEP: And User fills VISA_CTP one time password')
+    vctp_page = context.page_factory.get_page(Pages.VISA_CTP_PAGE)
+    vctp_page.fill_otp_field_and_check()
 
 
 @then('User will see that VISA_CTP payment was (?P<param>.+)')
@@ -42,11 +60,6 @@ def step_impl(context, param):
 @step('User selects (?P<string>.+) card on VISA_CTP popup')
 def step_impl(context, string):
     raise NotImplementedError(u'STEP: And User selects <string> card on VISA_CTP popup')
-
-
-@step('User login to VISA_CTP account with valid e-mail address')
-def step_impl(context):
-    raise NotImplementedError(u'STEP: And User login to VISA_CTP account with valid e-mail address')
 
 
 @step("User closes VISA_CTP checkout page")
--- tests/pages/locators/visa_ctp_locators.py
+++ tests/pages/locators/visa_ctp_locators.py
@@ -7,13 +7,25 @@ from selenium.webdriver.common.by import By
 class VisaClickToPayLocators:
     # pylint: disable=too-many-instance-attributes
 
+    # VCTP example page
+    card_number_input: By = (By.ID, 'st-form-card-number')
+    expiry_month_select: By = (By.ID, 'st-form-expiry-date-month')
+    expiry_year_select: By = (By.ID, 'st-form-expiry-date-year')
+    security_code_input: By = (By.ID, 'st-form-security-code')
+    look_up_my_cards_btn: By = (By.ID, 'st-ctp-lookup-btn')
+    register_card_checkbox: By = (By.ID, 'register')
+    submit_button: By = (By.ID, 'merchant-submit-button')
+
     @classmethod
     def get_billing_details_field_locator(cls, billing_field) -> By.ID:
         return By.ID, f'st-form-{billing_field}'
 
-    card_number_input: By = (By.ID, 'st-form-card-number')
-    expiry_date_input: By = (By.ID, 'st-form-expiry-date')
-    security_code_input: By = (By.ID, 'st-form-security-code')
-    look_up_my_cards_btn: By = (By.ID, 'todo')
-    register_card_checkbox: By = (By.ID, 'register')
-    submit_button: By = (By.ID, 'merchant-submit-button')
+    @classmethod
+    def get_delivery_details_field_locator(cls, delivery_field) -> By.ID:
+        return By.ID, f'st-form-{delivery_field}-d'
+
+    # Visa modal
+    email_input: By = (By.NAME, 'st-ctp-email')
+    submit_email_btn: By = (By.XPATH, '//div[@id=\'st-modal-wrapper\']//button')
+    submit_otp_btn: By = (By.XPATH, '//div[@id=\'st-modal-wrapper\']//button')
+    otp_input: By = (By.XPATH, '//div[@id=\'st-modal-wrapper\']//input')
