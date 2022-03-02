39abf806b4d280d400d6fa486506247af9475e00
Marek Sakowicz (PGS Software) <msakowicz@pgs-soft.com>
2022-03-02 15:42:07 +0100

    update

--- tests/configuration.py
+++ tests/configuration.py
@@ -52,8 +52,8 @@ def load_config():
         'EXECUTABLE_PATH_EDGE_DRIVER': get_from_env('EXECUTABLE_PATH_EDGE_DRIVER', ''),
         'EXECUTABLE_PATH_IE_DRIVER': get_from_env('EXECUTABLE_PATH_IE_DRIVER', ''),
         'LOGGING_PREFS': {'browser': 'SEVERE'},
-        'VCTP_LOGIN': get_from_env('EMAIL_LOGIN', 'securetestpgs5@gmail.com'),
-        'VCTP_PASSWORD': get_from_env('EMAIL_PASSWORD', 'Securetestpgs1'),
+        'VCTP_LOGIN': get_from_env('EMAIL_LOGIN', ''),
+        'VCTP_PASSWORD': get_from_env('EMAIL_PASSWORD', ''),
     }
 
     return AttrDict(config)
