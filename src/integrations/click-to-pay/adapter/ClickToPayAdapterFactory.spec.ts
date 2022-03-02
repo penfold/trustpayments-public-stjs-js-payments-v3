0861c928a4acbae9ddb6b1725435f6440e4180ef
Piotr Dobrowolski <pdobrowolski@pgs-soft.com>
2022-03-02 08:15:51 +0000

    STJS-2934 Added proxy in adapter to find card srcname

--- src/integrations/click-to-pay/adapter/ClickToPayAdapterFactory.spec.ts
+++ src/integrations/click-to-pay/adapter/ClickToPayAdapterFactory.spec.ts
@@ -1,31 +1,23 @@
-import { instance, mock } from 'ts-mockito';
-import { IMessageBus } from '../../../application/core/shared/message-bus/IMessageBus';
-import { IFrameQueryingService } from '../../../shared/services/message-bus/interfaces/IFrameQueryingService';
-import { DigitalTerminal } from '../digital-terminal/DigitalTerminal';
+import { instance, mock, when } from 'ts-mockito';
+import { ContainerInstance } from 'typedi';
 import { ClickToPayAdapterFactory } from './ClickToPayAdapterFactory';
 import { ClickToPayAdapterName } from './ClickToPayAdapterName';
 import { HPPClickToPayAdapter } from './hpp-adapter/HPPClickToPayAdapter';
-import { HPPUserIdentificationService } from './hpp-adapter/HPPUserIdentificationService';
 
 describe('ClickToPayAdapterFactory()', () => {
+  const containerMock: ContainerInstance = mock(ContainerInstance);
+  const hPPClickToPayAdapterMock: HPPClickToPayAdapter = mock(HPPClickToPayAdapter);
+  const hPPClickToPayAdapterInstance: HPPClickToPayAdapter = instance(hPPClickToPayAdapterMock);
   let sut: ClickToPayAdapterFactory;
 
   beforeEach(() => {
-    const messageBus: IMessageBus = mock<IMessageBus>();
-    const frameQueryingServiceMock = mock<IFrameQueryingService>();
-    const digitalTerminalMock = mock(DigitalTerminal);
-    const userIdentificationServiceMock: HPPUserIdentificationService = mock(HPPUserIdentificationService);
-    sut = new ClickToPayAdapterFactory(
-      instance(digitalTerminalMock),
-      instance(messageBus),
-      instance(frameQueryingServiceMock),
-      instance(userIdentificationServiceMock)
-    );
+    sut = new ClickToPayAdapterFactory(instance(containerMock));
+    when(containerMock.get(HPPClickToPayAdapter)).thenReturn(hPPClickToPayAdapterInstance);
   });
 
   describe('create()', () => {
     it('should return instance of adapter class depending on input parameter', () => {
-      expect(sut.create(ClickToPayAdapterName.hpp)).toBeInstanceOf(HPPClickToPayAdapter);
+      expect(sut.create(ClickToPayAdapterName.hpp)).toBe(hPPClickToPayAdapterInstance);
     });
   });
 });
