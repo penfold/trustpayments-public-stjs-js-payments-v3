de03996f80017b9f9bea77cf68d2f1444c86b36d
Maciej Krawczyk <mackrawczyk@pgs-soft.com>
2022-03-16 15:36:55 +0100

    completed  dpatransactionoptions, temporarily disabled digitalcardid

--- example/html/pages/ctp/mastercard/mastercard.ts
+++ example/html/pages/ctp/mastercard/mastercard.ts
@@ -112,10 +112,32 @@ window.addEventListener('load', async () => {
     return vSrc.checkout({
       srcCorrelationId: srcProfileResult?.srcCorrelationId,
       srciTransactionId: srciTransactionId,
-      srcDigitalCardId: cardId,
+      // srcDigitalCardId: cardId,
       encryptedCard,
       idToken: profile?.idToken,
-      windowRef: '',
+      windowRef: this,
+      dpaTransactionOptions: {
+        transactionAmount: {
+          transactionAmount: '1.00',
+          transactionCurrencyCode: 'USD',
+        },
+        transactionType: 'PURCHASE',
+        dpaBillingPreference: 'FULL',
+        dpaShippingPreference: 'FULL',
+        dpaAcceptedBillingCountries: [],
+        dpaAcceptedShippingCountries: [],
+        dpaLocale: 'en_US',
+        consumerEmailAddressRequested: true,
+        consumerNameRequested: true,
+        consumerPhoneNumberRequested: true,
+        paymentOptions: {
+          dpaDynamicDataTtlMinutes: 15,
+          dynamicDataType: 'DYNAMIC_CARD_SECURITY_CODE',
+        },
+        isGuestCheckout: false,
+        payloadTypeIndicator: 'PAYMENT',
+      },
+      srciActionCode: 'NEW_USER',
     });
   }
 
@@ -149,7 +171,9 @@ window.addEventListener('load', async () => {
 
     // console.log('ENROLL', await vSrc.enrollCard({ encryptedCard: 'eyJraWQiOiIxNDkxMjMtc3JjLWZwYW4tZW5jcnlwdGlvbiIsImFsZyI6IlJTQS1PQUVQLTI1NiIsImVuYyI6IkExMjhHQ00ifQ.NOOIT0JU98z0lQbnlcEuaEPhc2uPmrJOsAtwjyQrBF8o2SpadRUXXsreDNZn8jhn970axAOSZVSmQyH8GYmKtP5bT4yLm4oWTueK4p1V3MS2wq6tvCNG2p833Nd69W7DAaotVLX9Und6xgwOS_GCEmbWDUZ-5imxDR7RxEVftzFjpyZwLz7klqLovwNmhC3wL7HeEH5mQguKphsiDKyhcCGyy3T94SrGARJRhxCeV69j_jJ88D2xRwlkYyUzeiIEGxWKfbyIRXKancIVj9kQ711A8NZOaULT6lqo6ahPXUXt0nxj1iq2WRg6L97LJibfQMxr-evYe8iJoptxqrYU6w.izwqEfJ-PL0PA6MY.eMF9gmlmunWPvrdqrMImQYm7ZqMvWbvKd9jMt3cm4ejGiv2hTXLhy5rTyzyNf6uw5rDknB4PlcaZ5MzrWiT58RlolnyoxO1kV48aIqBOa1xUjIZ1z6IunRW8PcKslJWgb8PMY9AzSlC44HT7bU6nbbIXDIqyAKsFJo_ray1dgq-wXpmPGGih1kyTVTEFQ-5VHvv5br7ZPbXF2DgvAPDx5R5L.SMEYhps-JGb5ZUGgJmCbFg' }));
     // console.log('ENROLL', await vSrc.enrollCard({ encryptedCard: await encode(collectCardDetails()) }));
-    console.log('CHECKOUT', await checkout(srciTransactionId, srcProfileResult));
+    checkout(srciTransactionId, srcProfileResult).then(function(res) {
+      console.log('CHECKOUT', res);
+    });
   }
 
   document.getElementById('click2pay').addEventListener('click', () => click2pay());
