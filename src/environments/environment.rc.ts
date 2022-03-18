3750a7172fc129c2a20c14f65fb864fcc5e6e156
Wojciech Grabowski (PGS Software) <wgrabowski@pgs-soft.com>
2022-03-18 17:05:43 +0100

    STJS-2934 Add ClickToPay entries to all environemnt files

--- src/environments/environment.rc.ts
+++ src/environments/environment.rc.ts
@@ -47,4 +47,14 @@ export const environment = {
   },
   SCRIPT_LOAD_TIMEOUT: 30000,
   FRAUD_CONTROL_TIMEOUT: 5000,
+  CLICK_TO_PAY: {
+    VISA: {
+      SRC_SDK_URL: {
+        SANDBOX: 'https://sandbox-assets.secure.checkout.visa.com/checkout-widget/resources/js/src-i-adapter/visaSdk.js',
+        PROD: 'https://sandbox-assets.secure.checkout.visa.com/checkout-widget/resources/js/src-i-adapter/visaSdk.js',
+      },
+      SRC_INITIATOR_ID: 'GSTIDU1J8I2NQRWAU7EL21puifGrG2BzgnL9XfBjMzwo9wmtM',
+      ENCRYPTION_KID: 'A5CHRN38V3PJ90ACENUH13CCVOyXIL7A8rC9xClvyZyxvMgrE',
+    },
+  },
 };
