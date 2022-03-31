a1c81e98680ed6a24c8ee10d526cd93b54cbc2e5
Piotr Dobrowolski <pdobrowolski@pgs-soft.com>
2022-03-31 12:12:01 +0200

    passed data to gateway

--- src/client/integrations/google-pay/google-pay-sdk-provider/GooglePayCallbacks.ts
+++ src/client/integrations/google-pay/google-pay-sdk-provider/GooglePayCallbacks.ts
@@ -1,11 +1,13 @@
 import { Observable, map } from 'rxjs';
 import { Service } from "typedi";
+import { DomMethods } from '../../../../application/core/shared/dom-methods/DomMethods';
 import { IConfig } from "../../../../shared/model/config/IConfig";
 import { ConfigProvider } from "../../../../shared/services/config-provider/ConfigProvider";
+import { GooglePayPaymentService } from '../GooglePayPaymentService';
 
 @Service()
 export class GooglePayCallbacks {
-  constructor (private configProvider: ConfigProvider) {}
+  constructor (private configProvider: ConfigProvider, private googlePayPaymentService: GooglePayPaymentService) {}
 
   getCallbacks(): Observable<any> {
     return this.configProvider.getConfig$().pipe(
@@ -16,56 +18,61 @@ export class GooglePayCallbacks {
     ))
   }
 
-  private processPayment(paymentData) {
-    let attempts = 0;
+  // private processPayment(paymentData) {
+  //   let attempts = 0;
 
-    return new Promise(function(resolve, reject) {
-      setTimeout(function() {
-        console.log(paymentData.paymentMethodData.tokenizationData.token);
+  //   return new Promise(function(resolve, reject) {
+  //     setTimeout(function() {
+  //       console.log(paymentData.paymentMethodData.tokenizationData.token);
   
-        if (attempts++ % 2 == 0) {
-          reject(new Error('Every other attempt fails, next one should succeed'));      
-        } else {
-          resolve({}); 
-        }
-      }, 500);
-    });
-  }
+  //       if (attempts++ % 2 == 0) {
+  //         reject(new Error('Every other attempt fails, next one should succeed'));      
+  //       } else {
+  //         resolve({}); 
+  //       }
+  //     }, 500);
+  //   });
+  // }
 
   onPaymentAuthorized(config: IConfig) {
-    console.warn(88, config)
     return (paymentData: any) => {
-      return new Promise(function(resolve, reject) {
-        resolve({transactionState: 'SUCCESS'});
-        this.processPayment(paymentData)
-          .then(function() {
-            resolve({transactionState: 'SUCCESS'});
-          })
-          .catch(function() {
-            resolve({
-              transactionState: 'ERROR',
-              error: {
-                intent: 'PAYMENT_AUTHORIZATION',
-                message: 'Insufficient funds',
-                reason: 'PAYMENT_DATA_INVALID'
-              }
-            });
-          });
-      });
+      const formData = DomMethods.parseForm(config.formId);
+      console.warn(11, formData)
+      console.warn(111, paymentData)
+      return this.googlePayPaymentService.processPayment(formData, paymentData);
+      // return new Promise(function(resolve, reject) {
+      //   console.log(paymentData);
+      //   this.processPayment(paymentData)
+      //     .then(function() {
+      //       resolve({transactionState: 'SUCCESS'});
+      //     })
+      //     .catch(function() {
+      //       resolve({
+      //         transactionState: 'ERROR',
+      //         error: {
+      //           intent: 'PAYMENT_AUTHORIZATION',
+      //           message: 'Insufficient funds',
+      //           reason: 'PAYMENT_DATA_INVALID'
+      //         }
+      //       });
+      //     });
+      // });
     }
   }
 
   onPaymentDataChanged(config: any) {
-    const { googlePay: { defaultSelectedOptionId, shippingOptions }} = config;
+    const { defaultSelectedOptionId, shippingOptions } = config.googlePay.paymentRequest.shippingOptionParameters;
     const displayItems = [...config.googlePay.paymentRequest.transactionInfo.displayItems];
 
     return (intermediatePaymentData: any) => {
       return new Promise(function(resolve, reject) {
-        let shippingOptionData = intermediatePaymentData.shippingOptionData;
+        // let shippingOptionData = intermediatePaymentData.shippingOptionData;
         let paymentDataRequestUpdate = {};
 
         if (intermediatePaymentData.callbackTrigger === "INITIALIZE" || intermediatePaymentData.callbackTrigger === "SHIPPING_ADDRESS") {
+          console.log(defaultSelectedOptionId, shippingOptions);
           (paymentDataRequestUpdate as any).newShippingOptionParameters = { defaultSelectedOptionId, shippingOptions };
+          console.warn('123', paymentDataRequestUpdate)
           // let selectedShippingOptionId = (paymentDataRequestUpdate as any).newShippingOptionParameters.defaultSelectedOptionId;
           const newTransactionInfo = config.googlePay.paymentRequest.transactionInfo;
           let totalPrice = 0.00;
