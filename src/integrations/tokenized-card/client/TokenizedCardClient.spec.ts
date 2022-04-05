a1bdced2d198b9c7c2685f94d60490853ca266ca
krzysztofRawski <krawski@pgs-soft.com>
2022-04-05 08:44:04 +0200

    STJS-3179 First commit

---
+++ src/integrations/tokenized-card/client/TokenizedCardClient.spec.ts
@@ -0,0 +1,22 @@
+describe('TokenizedCardClient', () => {
+  describe('on initialization', () => {
+    it.todo('should send the destroy event')
+
+    it.todo('should create a pay button')
+  })
+
+  describe('on init()', () => {
+    it.todo('should insert security code frame')
+
+    it.todo('should init pay button')
+
+    it.todo('should add click handler')
+
+    it.todo('should enable pay button')
+
+    it.todo('should prevent the form from submit')
+
+    it.todo('should set disable listener')
+  })
+})
+
