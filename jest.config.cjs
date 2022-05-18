d2d4ea600df3ea5d32b0097d5a3c4e21a83416ef
tchomicz <tchomicz@pgs-soft.com>
2022-05-18 14:52:31 +0200

    update jest 28.1

--- jest.config.cjs
+++ jest.config.cjs
@@ -178,6 +178,11 @@ module.exports = {
 
   // An array of regexp pattern strings that are matched against all source file paths, matched files will skip transformation
   transformIgnorePatterns: ['node_modules'],
+  moduleNameMapper: {
+    'rxjs': '<rootDir>/node_modules/rxjs',
+    '.+\\.(css|styl|less|sass|scss|png|jpg|ttf|woff|woff2|svg)$': 'jest-transform-stub',
+  }
+
 
   // An array of regexp pattern strings that are matched against all modules before the module loader will automatically return a mock for them
   // unmockedModulePathPatterns: undefined,
