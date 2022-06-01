93ebc6df86c00edbd00ec0330f7cd425ca4ba851
tchomicz <tchomicz@pgs-soft.com>
2022-06-01 09:46:57 +0200

    STJS-3462 filter apple pay error

--- src/application/core/services/payments/ExceptionsArray.ts
+++ src/application/core/services/payments/ExceptionsArray.ts
@@ -1,3 +1,4 @@
 export const exceptionsArray = [
   'ApplePay not available: Works only on Safari',
+  'ApplePay not available: Your device does not support making payments with Apple Pay',
 ];
--- src/shared/services/sentry/constants/SentryBrowserOptions.ts
+++ src/shared/services/sentry/constants/SentryBrowserOptions.ts
@@ -1,8 +1,10 @@
 import { BrowserOptions } from '@sentry/browser';
 import { environment } from '../../../../environments/environment';
+import { SENTRY_EXCEPTION_MESSAGE_LIST } from '../SentryEventFiltering/SentryEventFilteringConfig';
 
 export const SENTRY_INIT_BROWSER_OPTIONS : Partial<BrowserOptions> = {
   sampleRate: environment.SENTRY.SAMPLE_RATE,
   attachStacktrace: true,
+  ignoreErrors: SENTRY_EXCEPTION_MESSAGE_LIST,
   normalizeDepth: 4,
 }
