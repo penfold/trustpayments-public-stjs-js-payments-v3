6c348867930285f179c1d40939359fa95a442a84
Krzysztof Chudy <kchudy-macwro@MACBOOK-PRO-JOPALUCH-MACWRO.lan>
2022-07-06 17:01:57 +0200

    add jwt parser tool

---
+++ tests/utils/helpers/jwt_decoder.py
@@ -0,0 +1,38 @@
+import jwt
+import json
+
+
+def decode_jwt(jwt_string):
+    """
+    DECODE JWT AND RETURNS JSON
+    """
+    decoded = jwt.decode(jwt_string, options={"verify_signature": False}, algorithms=["HS256"])
+
+    return json.loads(json.dumps(decoded))
+
+
+jwt_src = "TYPE_JWT_HERE"
+
+# DECODE AND READ JSON
+json_file = decode_jwt(jwt_src)
+
+# SAVE TIMESTAMP
+timestamp = json_file["payload"]["response"][0]["transactionstartedtimestamp"]
+
+# DECODE CHECKOUT RESPONSE JWT
+wallet_token = decode_jwt(json_file["payload"]["jwt"])["payload"]["wallettoken"]
+response = json.loads(wallet_token)["checkoutResponse"]
+checkout_response = json.loads(json.dumps(jwt.decode(response,
+                                                     options={"verify_signature": False},
+                                                     algorithms=["HS256"])))
+
+# SAVE CORRELATION ID AND TRANSACTION ID
+correlation_id = checkout_response["srcCorrelationId"]
+transaction_id = checkout_response["srciTransactionId"]
+
+
+# PRINT DATA
+print("transactionstartedtimestamp: " + timestamp)
+print("srcCorrelationId: " + correlation_id)
+print("srciTransactionId: " + transaction_id)
+
